fc258a2a774c0ace492ba098348f983b504b27cc
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-09-06 09:01:21 +0000

    Nuevos mensajes y parametros de excepciones
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@40089 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/common/error/ServiceExceptionParameters.java
@@ -0,0 +1,25 @@
+package org.siemac.metamac.statistical.resources.core.common.error;
+
+import org.siemac.metamac.statistical.resources.core.enume.domain.StatisticalResourceProcStatusEnum;
+
+public class ServiceExceptionParameters {
+
+    public static final String DATASET                            = "dataset";
+
+    public static final String IDENTIFIER                         = "identifier";
+
+    // DATASET
+    public static final String DATASET_TITLE                      = DATASET + ".title";
+    public static final String DATASET_OPERATION                  = DATASET + ".operation";
+
+    // LIFECYCLE
+    public static final String PROC_STATUS_DRAFT                  = StatisticalResourceProcStatusEnum.DRAFT.name();
+    public static final String PROC_STATUS_PRODUCTION_VALIDATION  = StatisticalResourceProcStatusEnum.PRODUCTION_VALIDATION.name();
+    public static final String PROC_STATUS_DIFFUSION_VALIDATION   = StatisticalResourceProcStatusEnum.DIFFUSION_VALIDATION.name();
+    public static final String PROC_STATUS_VALIDATION_REJECTED    = StatisticalResourceProcStatusEnum.VALIDATION_REJECTED.name();
+    public static final String PROC_STATUS_PUBLICATION_PROGRAMMED = StatisticalResourceProcStatusEnum.PUBLICATION_PROGRAMMED.name();
+    public static final String PROC_STATUS_PUBLICATION_PENDING    = StatisticalResourceProcStatusEnum.PUBLICATION_PENDING.name();
+    public static final String PROC_STATUS_PUBLISHED              = StatisticalResourceProcStatusEnum.PUBLISHED.name();
+    public static final String PROC_STATUS_PUBLICATION_FAILED     = StatisticalResourceProcStatusEnum.PUBLICATION_FAILED.name();
+    public static final String PROC_STATUS_ARCHIVED               = StatisticalResourceProcStatusEnum.ARCHIVED.name();
+}
--- metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/common/error/ServiceExceptionType.java
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/common/error/ServiceExceptionType.java
@@ -2,8 +2,12 @@ package org.siemac.metamac.statistical.resources.core.common.error;
 
 import org.siemac.metamac.core.common.exception.CommonServiceExceptionType;
 
-public class ServiceExceptionType extends  CommonServiceExceptionType {
+public class ServiceExceptionType extends CommonServiceExceptionType {
 
-    public static final CommonServiceExceptionType SECURITY_ACTION_NOT_ALLOWED      = create("exception.statistical.resources.security.action_not_allowed");
+    public static final CommonServiceExceptionType SECURITY_ACTION_NOT_ALLOWED                 = create("exception.resources.security.action_not_allowed");
+
+    public static final CommonServiceExceptionType DATASET_NOT_FOUND                           = create("exception.resources.dataset.not_found");
+    public static final CommonServiceExceptionType DATASET_ALREADY_EXIST_IDENTIFIER_DUPLICATED = create("exception.resources.dataset.already_exist.identifier_duplicated");
+
 
 }
