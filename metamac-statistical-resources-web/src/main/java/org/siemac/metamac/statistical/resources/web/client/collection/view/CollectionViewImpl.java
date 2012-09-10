1bb04fda0a61832e3b72baac3bd0ec8f68be397c
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-09-10 15:10:36 +0000

    METAMAC-1050 Metadatos de una colección
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@40166 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/collection/view/CollectionViewImpl.java
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/collection/view/CollectionViewImpl.java
@@ -1,25 +1,45 @@
 package org.siemac.metamac.statistical.resources.web.client.collection.view;
 
+import static org.siemac.metamac.statistical.resources.web.client.StatisticalResourcesWeb.getConstants;
+
 import org.siemac.metamac.statistical.resources.core.dto.CollectionDto;
 import org.siemac.metamac.statistical.resources.web.client.collection.presenter.CollectionPresenter;
+import org.siemac.metamac.statistical.resources.web.client.collection.utils.CollectionClientSecurityUtils;
 import org.siemac.metamac.statistical.resources.web.client.collection.view.handlers.CollectionUiHandlers;
+import org.siemac.metamac.statistical.resources.web.client.collection.widgets.CollectionMainFormLayout;
 import org.siemac.metamac.statistical.resources.web.client.enums.StatisticalResourcesToolStripButtonEnum;
+import org.siemac.metamac.web.common.client.utils.InternationalStringUtils;
+import org.siemac.metamac.web.common.client.widgets.form.GroupDynamicForm;
 
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewImpl;
 import com.smartgwt.client.types.Overflow;
 import com.smartgwt.client.widgets.Canvas;
-import com.smartgwt.client.widgets.Label;
+import com.smartgwt.client.widgets.events.ClickEvent;
+import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.layout.VLayout;
 import com.smartgwt.client.widgets.toolbar.ToolStrip;
 import com.smartgwt.client.widgets.toolbar.ToolStripButton;
 
 public class CollectionViewImpl extends ViewImpl implements CollectionPresenter.CollectionView {
 
-    private CollectionUiHandlers uiHandlers;
+    private CollectionUiHandlers     uiHandlers;
 
-    private VLayout              panel;
+    private VLayout                  panel;
+    private CollectionMainFormLayout mainFormLayout;
+
+    private GroupDynamicForm         identifiersForm;
+    private GroupDynamicForm         versionForm;
+    private GroupDynamicForm         lifeCycleForm;
+    private GroupDynamicForm         contentMetadataForm;
+
+    private GroupDynamicForm         identifiersEditionForm;
+    private GroupDynamicForm         versionEditionForm;
+    private GroupDynamicForm         lifeCycleEditionForm;
+    private GroupDynamicForm         contentMetadataEditionForm;
+
+    private CollectionDto            collectionDto;
 
     @Inject
     public CollectionViewImpl() {
@@ -27,7 +47,13 @@ public class CollectionViewImpl extends ViewImpl implements CollectionPresenter.
         panel = new VLayout();
         panel.setHeight100();
         panel.setOverflow(Overflow.SCROLL);
-        panel.addMember(new Label("collection"));
+
+        mainFormLayout = new CollectionMainFormLayout(CollectionClientSecurityUtils.canUpdateCollection());
+        bindMainFormLayoutEvents();
+        createViewForm();
+        createEditionForm();
+
+        panel.addMember(mainFormLayout);
     }
 
     @Override
@@ -63,8 +89,95 @@ public class CollectionViewImpl extends ViewImpl implements CollectionPresenter.
 
     @Override
     public void setCollection(CollectionDto collectionDto) {
-        // TODO Auto-generated method stub
+        this.collectionDto = collectionDto;
 
+        mainFormLayout.setTitleLabelContents(InternationalStringUtils.getLocalisedString(collectionDto.getTitle()));
+        mainFormLayout.setViewMode();
+
+        setCollectionViewMode(collectionDto);
+        setCollectionEditionMode(collectionDto);
+    }
+
+    private void bindMainFormLayoutEvents() {
+        mainFormLayout.getTranslateToolStripButton().addClickHandler(new ClickHandler() {
+
+            @Override
+            public void onClick(ClickEvent event) {
+                boolean translationsShowed = mainFormLayout.getTranslateToolStripButton().isSelected();
+                identifiersForm.setTranslationsShowed(translationsShowed);
+                identifiersEditionForm.setTranslationsShowed(translationsShowed);
+
+                versionForm.setTranslationsShowed(translationsShowed);
+                versionEditionForm.setTranslationsShowed(translationsShowed);
+
+                lifeCycleForm.setTranslationsShowed(translationsShowed);
+                lifeCycleEditionForm.setTranslationsShowed(translationsShowed);
+
+                contentMetadataForm.setTranslationsShowed(translationsShowed);
+                contentMetadataEditionForm.setTranslationsShowed(translationsShowed);
+            }
+        });
+
+        // Save
+        mainFormLayout.getSave().addClickHandler(new ClickHandler() {
+
+            @Override
+            public void onClick(ClickEvent event) {
+                if (identifiersEditionForm.validate(false) && versionEditionForm.validate(false) && lifeCycleEditionForm.validate(false) && contentMetadataEditionForm.validate(false)) {
+                    uiHandlers.saveCollection(getCollectionDto());
+                }
+            }
+        });
+    }
+
+    private void createViewForm() {
+        // Identifiers form
+        identifiersForm = new GroupDynamicForm(getConstants().collectionIdentifiers());
+
+        // Version form
+        versionForm = new GroupDynamicForm(getConstants().collectionVersion());
+
+        // Life cycle form
+        lifeCycleForm = new GroupDynamicForm(getConstants().collectionLifeCycle());
+
+        // Content metadata form
+        contentMetadataForm = new GroupDynamicForm(getConstants().collectionContentMetadata());
+
+        mainFormLayout.addViewCanvas(identifiersForm);
+        mainFormLayout.addViewCanvas(versionForm);
+        mainFormLayout.addViewCanvas(lifeCycleForm);
+        mainFormLayout.addViewCanvas(contentMetadataForm);
+    }
+
+    private void createEditionForm() {
+        // Identifiers form
+        identifiersEditionForm = new GroupDynamicForm(getConstants().collectionIdentifiers());
+
+        // Version form
+        versionEditionForm = new GroupDynamicForm(getConstants().collectionVersion());
+
+        // Life cycle form
+        lifeCycleEditionForm = new GroupDynamicForm(getConstants().collectionLifeCycle());
+
+        // Content metadata form
+        contentMetadataEditionForm = new GroupDynamicForm(getConstants().collectionContentMetadata());
+
+        mainFormLayout.addEditionCanvas(identifiersEditionForm);
+        mainFormLayout.addEditionCanvas(versionEditionForm);
+        mainFormLayout.addEditionCanvas(lifeCycleEditionForm);
+        mainFormLayout.addEditionCanvas(contentMetadataEditionForm);
+    }
+
+    private void setCollectionViewMode(CollectionDto collectionDto) {
+
+    }
+
+    private void setCollectionEditionMode(CollectionDto collectionDto) {
+
+    }
+
+    private CollectionDto getCollectionDto() {
+        return collectionDto;
     }
 
 }
---
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/collection/widgets/CollectionMainFormLayout.java
@@ -0,0 +1,21 @@
+package org.siemac.metamac.statistical.resources.web.client.collection.widgets;
+
+import org.siemac.metamac.web.common.client.widgets.form.InternationalMainFormLayout;
+
+public class CollectionMainFormLayout extends InternationalMainFormLayout {
+
+    public CollectionMainFormLayout() {
+        super();
+        common();
+    }
+
+    public CollectionMainFormLayout(boolean canEdit) {
+        super(canEdit);
+        common();
+    }
+
+    private void common() {
+
+    }
+
+}
