78d7bc5ede891512c17163b857d747a55f56331d
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-09-10 07:29:32 +0000

    METAMAC-1045 Collection constants, record and listGrid
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@40151 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/collection/widgets/CollectionListGrid.java
@@ -0,0 +1,60 @@
+package org.siemac.metamac.statistical.resources.web.client.collection.widgets;
+
+import static org.siemac.metamac.statistical.resources.web.client.StatisticalResourcesWeb.getConstants;
+
+import java.util.List;
+
+import org.siemac.metamac.statistical.resources.core.dto.CollectionDto;
+import org.siemac.metamac.statistical.resources.web.client.collection.model.ds.CollectionDS;
+import org.siemac.metamac.statistical.resources.web.client.collection.model.record.CollectionRecord;
+import org.siemac.metamac.statistical.resources.web.client.utils.RecordUtils;
+import org.siemac.metamac.web.common.client.widgets.BaseCustomListGrid;
+
+import com.smartgwt.client.widgets.grid.HoverCustomizer;
+import com.smartgwt.client.widgets.grid.ListGridField;
+import com.smartgwt.client.widgets.grid.ListGridRecord;
+
+public class CollectionListGrid extends BaseCustomListGrid {
+
+    public CollectionListGrid() {
+        super();
+
+        this.setShowAllRecords(true);
+
+        ListGridField identifierField = new ListGridField(CollectionDS.IDENTIFIER, getConstants().collectionIdentifier());
+        ListGridField titleField = new ListGridField(CollectionDS.TITLE, getConstants().collectionTitle());
+
+        // ToolTip
+        identifierField.setShowHover(true);
+        identifierField.setHoverCustomizer(new HoverCustomizer() {
+
+            @Override
+            public String hoverHTML(Object value, ListGridRecord record, int rowNum, int colNum) {
+                CollectionRecord collectionRecord = (CollectionRecord) record;
+                return collectionRecord.getIdentifier();
+            }
+        });
+        titleField.setShowHover(true);
+        titleField.setHoverCustomizer(new HoverCustomizer() {
+
+            @Override
+            public String hoverHTML(Object value, ListGridRecord record, int rowNum, int colNum) {
+                CollectionRecord collectionRecord = (CollectionRecord) record;
+                return collectionRecord.getName();
+            }
+        });
+        this.setFields(identifierField, titleField);
+    }
+
+    public void setCollections(List<CollectionDto> collections) {
+        removeAllData();
+        if (collections != null) {
+            CollectionRecord[] records = new CollectionRecord[collections.size()];
+            for (int i = 0; i < collections.size(); i++) {
+                records[i] = RecordUtils.getCollectionRecord(collections.get(i));
+            }
+            setData(records);
+        }
+    }
+
+}
---
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/model/ds/LifeCycleStatisticalResourceDS.java
@@ -0,0 +1,49 @@
+package org.siemac.metamac.statistical.resources.web.client.model.ds;
+
+import com.smartgwt.client.data.DataSource;
+import com.smartgwt.client.data.fields.DataSourceIntegerField;
+
+public class LifeCycleStatisticalResourceDS extends DataSource {
+
+    // IDENTITY
+    public static final String ID                          = "lc-id";
+    // STATISTICAL RESOURCE
+    public static final String OPERATION                   = "lc-op";
+    // AUDITABLE
+    // public static final String CREATOR_AUDIT = "lc-creator-audit";
+    // public static final String DATE_CREATED = "lc-date-creation";
+    // public static final String LAST_UPDATE_USER = "lc-update-user";
+    // public static final String DATE_LAST_UPDATE = "lc-last-update";
+    // IDENTIFIABLE
+    public static final String IDENTIFIER                  = "lc-identifier";
+    public static final String URI                         = "lc-uri";
+    public static final String URN                         = "lc-urn";
+    public static final String TITLE                       = "lc-title";
+    // VERSIONABLE
+    public static final String VERSION_LOGIC               = "lc-version";
+    public static final String DATE_VERSION                = "lc-date-version";
+    public static final String NEXT_VERSION                = "lc-next-version";
+    public static final String RATIONALE                   = "lc-rationale";
+    public static final String RATIONALE_TYPE              = "lc-rationale-type";
+    // LIFE CYCLE
+    public static final String RESPONSABILITY_CONTRIBUTOR  = "lc-resp-contr";
+    public static final String RESPONSABILITY_SUBMITTED    = "lc-resp-subm";
+    public static final String RESPONSABILITY_ACCEPTED     = "lc-resp-acc";
+    public static final String RESPONSABILITY_ISSUED       = "lc-resp-issued";
+    public static final String RESPONSABILITY_OUT_OF_PRINT = "lc-resp-oop";
+    public static final String CREATOR                     = "lc-creator";
+    public static final String CONTRIBUTOR                 = "lc-contr";
+    public static final String PUBLISHER                   = "lc-pub";
+    public static final String MEDIATOR                    = "lc-media";
+    public static final String DATE_SUBMITTED              = "lc-date-subm";
+    public static final String DATE_ACCEPTED               = "lc-date-acc";
+    public static final String DATE_ISSUED                 = "lc-date-issued";
+    public static final String PROC_STATUS                 = "lc-proc-status";
+
+    public LifeCycleStatisticalResourceDS() {
+        DataSourceIntegerField id = new DataSourceIntegerField(ID, "id");
+        id.setPrimaryKey(true);
+        addField(id);
+    }
+
+}
