a23cb86323d4385098d07a608c81ef4e576bcb46
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-11-06 16:15:30 +0000

    Formato
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@41520 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-core/src/main/java/org/siemac/metamac/core/common/utils/EntityMetadata.java
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/core/common/utils/EntityMetadata.java
@@ -4,20 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-
 public class EntityMetadata {
-    private String tableName;
-    private Map<String,String> properties;
-    
-    private Map<String,String> relationsDBName;
-    
-    private Map<String,String> inverseRelations; //key: field name, value:joincolumn
-    
-    private Map<String,JoinTableMetadata> joinTables;
-    
-    private String discriminatorColumn;
-    private boolean isSingleTable;
-    
+
+    private String                         tableName;
+    private Map<String, String>            properties;
+
+    private Map<String, String>            relationsDBName;
+
+    private Map<String, String>            inverseRelations;   // key: field name, value:joincolumn
+
+    private Map<String, JoinTableMetadata> joinTables;
+
+    private String                         discriminatorColumn;
+    private boolean                        isSingleTable;
+
     public EntityMetadata() {
         properties = new HashMap<String, String>();
         relationsDBName = new HashMap<String, String>();
@@ -26,99 +26,99 @@ public class EntityMetadata {
         discriminatorColumn = null;
         isSingleTable = false;
     }
-    
+
     public String getTableName() {
         return tableName;
     }
-    
+
     public Set<String> getAllAttributesAndRelations() {
         return properties.keySet();
     }
-    
+
     public Set<String> getJoinTableReferencesNames() {
         return joinTables.keySet();
     }
-    
+
     public EntityMetadata getEntityMetadataForJoinTable(String objName, Object value) {
         return joinTables.get(objName).generateEntityMetadata(getId(), value);
     }
-    
+
     public void addJoinTableReference(String objName, String joinTableName, String joinColumnName, String inverseJoinColumnName) {
         joinTables.put(objName, new JoinTableMetadata(joinTableName, joinColumnName, inverseJoinColumnName));
     }
-    
+
     public void embed(String prefix, EntityMetadata metadata) {
         for (String propName : metadata.properties.keySet()) {
-            this.properties.put(prefix+"_"+propName, metadata.properties.get(propName));
+            this.properties.put(prefix + "_" + propName, metadata.properties.get(propName));
         }
     }
-    
+
     public void setTableName(String tableName) {
         this.tableName = tableName;
     }
-    
-    public void setColumnValue(String columnName,Object value) {
+
+    public void setColumnValue(String columnName, Object value) {
         if (value != null) {
             properties.put(columnName, value.toString());
         } else {
             properties.put(columnName, null);
         }
     }
-    
+
     public void setRelationValue(String relationName, String id) {
         String dbColumn = relationsDBName.get(relationName);
         setColumnValue(dbColumn, id);
     }
-    
+
     public void addDiscriminatorColumn(String columnName) {
         discriminatorColumn = columnName;
         properties.put(columnName, null);
     }
-    
+
     public void setDiscriminatorColumnValue(String value) {
-        properties.put(discriminatorColumn,value);
+        properties.put(discriminatorColumn, value);
     }
-    
+
     public boolean hasDiscriminatorColumn() {
         return discriminatorColumn != null;
     }
-    
+
     public String getId() {
         return properties.get("ID");
     }
-    
+
     public void setId(Long id) {
         setColumnValue("ID", id);
     }
-    
+
     public void setSingleTable(boolean singleTable) {
         isSingleTable = singleTable;
     }
-    
+
     public boolean isSingleTable() {
         return isSingleTable;
     }
-    
+
     public void addInverseRelationNameMapping(String objectName, String joinColumn) {
         inverseRelations.put(objectName, joinColumn);
     }
-    
+
     public void addRelationNameMapping(String objectName, String databaseName) {
         relationsDBName.put(objectName, databaseName);
     }
-    
+
     public Set<String> getRelationsNames() {
         return relationsDBName.keySet();
     }
-    
+
     public Set<String> getInverseRelationsNames() {
         return inverseRelations.keySet();
     }
-    
+
     public String getInverseRelationNameJoinColumn(String objName) {
         return inverseRelations.get(objName);
     }
-    
+
     public String getXmlRepresentation(Set<String> attributes) {
         StringBuffer buffer = new StringBuffer();
         buffer.append("<").append(tableName.toUpperCase()).append("\n");
@@ -134,18 +134,19 @@ public class EntityMetadata {
         buffer.append("/>\n");
         return buffer.toString();
     }
-    
+
     private class JoinTableMetadata {
+
         private String tableName;
         private String joinColumnName;
         private String inverseJoinColumnName;
-        
+
         public JoinTableMetadata(String tableName, String joinColumnName, String inverseJoinColumnName) {
             this.inverseJoinColumnName = inverseJoinColumnName;
             this.tableName = tableName;
             this.joinColumnName = joinColumnName;
         }
-        
+
         public EntityMetadata generateEntityMetadata(Object joinColumnValue, Object inverseJoinColumnValue) {
             EntityMetadata metadata = new EntityMetadata();
             metadata.setTableName(tableName);
