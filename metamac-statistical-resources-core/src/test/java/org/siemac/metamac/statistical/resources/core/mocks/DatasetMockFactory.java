1a4f2fe1feee229c8cfce50c4591d67f6f27e75b
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-11-08 14:33:02 +0000

    Realización de tests de repositorio de DatasetVersion
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@41640 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-core/src/test/java/org/siemac/metamac/statistical/resources/core/mocks/DatasetMockFactory.java
+++ metamac-statistical-resources-core/src/test/java/org/siemac/metamac/statistical/resources/core/mocks/DatasetMockFactory.java
@@ -1,5 +1,6 @@
 package org.siemac.metamac.statistical.resources.core.mocks;
 
+import static org.siemac.metamac.statistical.resources.core.mocks.DatasetVersionMockFactory.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,7 +18,7 @@ public class DatasetMockFactory extends MockFactory<Dataset> {
     public static final Dataset         DATASET_02_BASIC                              = StatisticalResourcesDoMocks.mockDataset();
 
     public static final String          DATASET_03_BASIC_WITH_2_DATASET_VERSIONS_NAME = "DATASET_03_BASIC_WITH_2_DATASET_VERSIONS";
-    public static final Dataset         DATASET_03_BASIC_WITH_2_DATASET_VERSIONS      = StatisticalResourcesDoMocks.mockDataset();
+    public static final Dataset         DATASET_03_BASIC_WITH_2_DATASET_VERSIONS      = createDataset03With2DatasetVersions();
 
     private static Map<String, Dataset> mocks;
 
@@ -30,4 +31,12 @@ public class DatasetMockFactory extends MockFactory<Dataset> {
     public Dataset getMock(String id) {
         return mocks.get(id);
     }
+
+    private static Dataset createDataset03With2DatasetVersions() {
+        Dataset dataset = StatisticalResourcesDoMocks.mockDataset();
+        dataset.addVersion(DATASET_VERSION_03_ASSOCIATED_WITH_DATASET_03);
+        dataset.addVersion(DATASET_VERSION_04_ASSOCIATED_WITH_DATASET_03_AND_LAST_VERSION);
+        return dataset;
+    }
+    
 }
