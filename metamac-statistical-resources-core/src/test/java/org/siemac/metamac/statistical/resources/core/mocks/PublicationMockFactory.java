df80273ffb5325a1530b3a10c9fef9ccb6066130
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-02-01 13:41:46 +0000

    quitados autowired innecesarios
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@43307 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-core/src/test/java/org/siemac/metamac/statistical/resources/core/mocks/PublicationMockFactory.java
+++ metamac-statistical-resources-core/src/test/java/org/siemac/metamac/statistical/resources/core/mocks/PublicationMockFactory.java
@@ -1,23 +1,19 @@
 package org.siemac.metamac.statistical.resources.core.mocks;
 
 import org.siemac.metamac.statistical.resources.core.publication.domain.Publication;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PublicationMockFactory extends StatisticalResourcesMockFactory<Publication> {
 
-    @Autowired
-    PublicationVersionMockFactory publicationVersionMockFactory;
+    public static final String PUBLICATION_01_BASIC_NAME                             = "PUBLICATION_01_BASIC";
+    private static Publication PUBLICATION_01_BASIC;
 
-    public static final String    PUBLICATION_01_BASIC_NAME                             = "PUBLICATION_01_BASIC";
-    private static Publication    PUBLICATION_01_BASIC;
+    public static final String PUBLICATION_02_BASIC_WITH_GENERATED_VERSION_NAME      = "PUBLICATION_02_BASIC_WITH_GENERATED_VERSION";
+    private static Publication PUBLICATION_02_BASIC_WITH_GENERATED_VERSION;
 
-    public static final String    PUBLICATION_02_BASIC_WITH_GENERATED_VERSION_NAME      = "PUBLICATION_02_BASIC_WITH_GENERATED_VERSION";
-    private static Publication    PUBLICATION_02_BASIC_WITH_GENERATED_VERSION;
-
-    public static final String    PUBLICATION_03_BASIC_WITH_2_PUBLICATION_VERSIONS_NAME = "PUBLICATION_03_BASIC_WITH_2_PUBLICATION_VERSIONS";
-    private static Publication    PUBLICATION_03_BASIC_WITH_2_PUBLICATION_VERSIONS;
+    public static final String PUBLICATION_03_BASIC_WITH_2_PUBLICATION_VERSIONS_NAME = "PUBLICATION_03_BASIC_WITH_2_PUBLICATION_VERSIONS";
+    private static Publication PUBLICATION_03_BASIC_WITH_2_PUBLICATION_VERSIONS;
 
     public static Publication getPublication01Basic() {
         if (PUBLICATION_01_BASIC == null) {
