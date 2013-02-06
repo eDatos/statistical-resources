9e4f5d69f0968bced2e6d249cd02c238d20ad2c1
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-02-06 16:10:46 +0000

    METAMAC-1516 - [Test] Refactor de la clase MetamacBaseTest para que pase a ser MetamacDBUnitBaseTest
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@43484 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-core/src/test/java/org/siemac/metamac/statistical/resources/core/StatisticalResourcesBaseTest.java
+++ metamac-statistical-resources-core/src/test/java/org/siemac/metamac/statistical/resources/core/StatisticalResourcesBaseTest.java
@@ -1,15 +1,10 @@
 package org.siemac.metamac.statistical.resources.core;
 
-import static org.junit.Assert.assertEquals;
-
 import org.apache.commons.lang.StringUtils;
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
 import org.junit.Rule;
-import org.junit.rules.ExpectedException;
-import org.siemac.metamac.core.common.exception.MetamacException;
-import org.siemac.metamac.core.common.exception.MetamacExceptionItem;
+import org.siemac.metamac.common.test.MetamacBaseTest;
+import org.siemac.metamac.common.test.dbunit.MetamacDBUnitBaseTests.DataBaseProvider;
 import org.siemac.metamac.sso.client.MetamacPrincipal;
 import org.siemac.metamac.sso.client.MetamacPrincipalAccess;
 import org.siemac.metamac.sso.client.SsoClientConstants;
@@ -18,7 +13,7 @@ import org.siemac.metamac.statistical.resources.core.enume.domain.StatisticalRes
 import org.siemac.metamac.statistical.resources.core.utils.mocks.configuration.MockAnnotationRule;
 import org.springframework.beans.factory.annotation.Value;
 
-public abstract class StatisticalResourcesBaseTest {
+public abstract class StatisticalResourcesBaseTest extends MetamacBaseTest {
 
     protected static String      EMPTY             = StringUtils.EMPTY;
 
@@ -30,17 +25,13 @@ public abstract class StatisticalResourcesBaseTest {
     @Value("${metamac.statistical_resources.db.provider}")
     private String               databaseProvider;
 
-    private final ServiceContext serviceContext    = new ServiceContext("junit", "junit", "app");
-
-    @Rule
-    public ExpectedException  expectedException = ExpectedException.none();
-
-    public ServiceContext getServiceContextWithoutPrincipal() {
-        return serviceContext;
-    }
-
     @Rule
     public MockAnnotationRule mockRule = new MockAnnotationRule();
+
+    
+    protected ServiceContext getServiceContextWithoutPrincipal() {
+        return mockServiceContextWithoutPrincipal();
+    }
 
     protected ServiceContext getServiceContextAdministrador() {
         ServiceContext serviceContext = getServiceContextWithoutPrincipal();
@@ -60,38 +51,9 @@ public abstract class StatisticalResourcesBaseTest {
         metamacPrincipal.getAccesses().add(new MetamacPrincipalAccess(role.getName(), StatisticalResourcesConstants.SECURITY_APPLICATION_ID, null));
         serviceContext.setProperty(SsoClientConstants.PRINCIPAL_ATTRIBUTE, metamacPrincipal);
     }
-
-    protected void expectedMetamacException(final MetamacException testExpectedException, final int testExpectedExceptionItems) {
-        expectedException.expect(MetamacException.class);
-        expectedException.expect(new BaseMatcher<MetamacException>() {
-
-            @Override
-            public boolean matches(Object e) {
-                MetamacException metamacCoreException = (MetamacException) e;
-                
-                assertEquals(testExpectedExceptionItems, metamacCoreException.getExceptionItems().size());
-                
-                for (int i = 0; i < metamacCoreException.getExceptionItems().size(); i++) {
-                    assertEqualsMetamacExceptionItem(testExpectedException.getExceptionItems().get(i), metamacCoreException.getExceptionItems().get(i));
-                }
-                
-                return testExpectedException.getExceptionItems().get(0).getCode().equals(metamacCoreException.getExceptionItems().get(0).getCode());
-            }
-
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("exception = " + testExpectedException + "");
-            }
-        });
-    }
     
-    private static void assertEqualsMetamacExceptionItem(MetamacExceptionItem expected, MetamacExceptionItem actual) {
-        assertEquals(expected.getCode(), actual.getCode());
-        assertEquals(expected.getMessage(), actual.getMessage());
-        assertEquals(expected.getMessageParameters().length, actual.getMessageParameters().length);
-        
-        for (int i = 0; i < expected.getMessageParameters().length; i++) {
-            assertEquals(expected.getMessageParameters()[i], actual.getMessageParameters()[i]);
-        }
+    @Override
+    protected DataBaseProvider getDatabaseProvider() {
+        return DataBaseProvider.valueOf(databaseProvider);
     }
 }
