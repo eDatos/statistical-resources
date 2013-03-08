942eeaf429b526ce20eec1e266e795269bba49a6
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-03-08 12:37:19 +0000

    Modificada la consulta de obtención del último código por motivos de eficiencia
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@44178 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/dataset/repositoryimpl/DatasetRepositoryImpl.java
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/dataset/repositoryimpl/DatasetRepositoryImpl.java
@@ -1,8 +1,5 @@
 package org.siemac.metamac.statistical.resources.core.dataset.repositoryimpl;
 
-
-import java.util.List;
-
 import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
@@ -12,27 +9,19 @@ import org.springframework.stereotype.Repository;
  */
 @Repository("datasetRepository")
 public class DatasetRepositoryImpl extends DatasetRepositoryBase {
+
     public DatasetRepositoryImpl() {
     }
 
     @Override
     public String findLastDatasetCode(String operationUrn) {
 
-        String subCodeHql = "substring(dsv.siemacMetadataStatisticalResource.code, length(dsv.siemacMetadataStatisticalResource.code)-3, length(dsv.siemacMetadataStatisticalResource.code))";  
-        String hql = "select distinct(" + subCodeHql + ")" +
-                     "from DatasetVersion as dsv " +
-                     "where dsv.siemacMetadataStatisticalResource.statisticalOperation.urn = :operationUrn " +
-                     "order by " + subCodeHql + " desc";
-        
-        
+        String subCodeHql = "substring(dsv.siemacMetadataStatisticalResource.code, length(dsv.siemacMetadataStatisticalResource.code)-3, length(dsv.siemacMetadataStatisticalResource.code))";
+        String hql = "select max(" + subCodeHql + ")" + "from DatasetVersion as dsv " + "where dsv.siemacMetadataStatisticalResource.statisticalOperation.urn = :operationUrn ";
+
         Query query = getEntityManager().createQuery(hql);
-        query.setMaxResults(1);
         query.setParameter("operationUrn", operationUrn);
-        List results = query.getResultList();
-        if (results != null && results.size() > 0) {
-            return results.get(0).toString();
-        } else {
-            return null;
-        }
+        String result = (String) query.getSingleResult();
+        return result;
     }
 }
