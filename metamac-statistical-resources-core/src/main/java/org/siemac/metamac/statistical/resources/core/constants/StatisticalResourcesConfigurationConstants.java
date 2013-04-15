62f5f700ac7d323cfbd0103d1664b5d7a6c50a3b
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-04-15 12:42:04 +0000

    METAMAC-1564 Listener para comprobar propiedades de configuración del data
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@45132 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/constants/StatisticalResourcesConfigurationConstants.java
@@ -0,0 +1,20 @@
+package org.siemac.metamac.statistical.resources.core.constants;
+
+import org.siemac.metamac.core.common.constants.shared.ConfigurationConstants;
+
+public class StatisticalResourcesConfigurationConstants extends ConfigurationConstants {
+
+    // PROPERTIES SPECIFIED IN THE DATA DIRECTORY
+
+    // Configuration
+
+    public static final String USER_GUIDE_FILE_NAME = "metamac.statistical.resources.user.guide.file.name";
+
+    // DataSource
+
+    public static final String DB_URL               = "metamac.statistical.resources.db.url";
+    public static final String DB_USERNAME          = "metamac.statistical.resources.db.username";
+    public static final String DB_PASSWORD          = "metamac.statistical.resources.db.password";
+    public static final String DB_DIALECT           = "metamac.statistical.resources.db.dialect";
+    public static final String DB_DRIVER_NAME       = "metamac.statistical.resources.db.driverName";
+}
--- metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/constants/StatisticalResourcesConstants.java
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/constants/StatisticalResourcesConstants.java
@@ -3,7 +3,4 @@ package org.siemac.metamac.statistical.resources.core.constants;
 public class StatisticalResourcesConstants {
 
     public static final String SECURITY_APPLICATION_ID = "GESTOR_RECURSOS_ESTADISTICOS";
-
-    // APIS
-    public static final String SRM_INTERNAL_ENDPOINT   = "metamac.statistical.resources.clients.srm.rest.internal";
 }
