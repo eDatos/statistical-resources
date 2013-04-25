5a51cc3034670b380195d20b63843189ddc89c17
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-04-25 07:15:25 +0000

    METAMAC-1605 - Servicios de ciclo de vida en core
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@45401 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-statistical-resources-core/src/test/java/org/siemac/metamac/statistical/resources/core/lifecycle/serviceapi/LifecycleServiceBaseTest.java
@@ -0,0 +1,18 @@
+package org.siemac.metamac.statistical.resources.core.lifecycle.serviceapi;
+
+/**
+ * Definition of test methods to implement.
+ */
+
+// IDEA: This class could be generated
+
+public interface LifecycleServiceBaseTest {
+
+    public void testSendToProductionValidation() throws Exception;
+
+    public void testSendToDiffusionValidation() throws Exception;
+
+    public void testSendToValidationRejected() throws Exception;
+
+    public void testSendToPublished() throws Exception;
+}
