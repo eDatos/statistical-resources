3bc7813ec998ddd509867ea5cf9eb35de6b10a64
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-05-13 13:05:49 +0000

    Nueva operacion de la api de srm para listar dsds
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@45836 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/invocation/SrmRestInternalService.java
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/invocation/SrmRestInternalService.java
@@ -1,9 +1,12 @@
 package org.siemac.metamac.statistical.resources.core.invocation;
 
 import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.DataStructure;
+import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.DataStructures;
 
 
 public interface SrmRestInternalService {
 
     DataStructure retrieveDsdByUrn(String urn);
+
+    DataStructures findDsds(int firstResult, int maxResult, String condition);
 }
--- metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/invocation/SrmRestInternalServiceImpl.java
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/invocation/SrmRestInternalServiceImpl.java
@@ -2,6 +2,7 @@ package org.siemac.metamac.statistical.resources.core.invocation;
 
 import org.siemac.metamac.core.common.util.GeneratorUrnUtils;
 import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.DataStructure;
+import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.DataStructures;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +19,13 @@ public class SrmRestInternalServiceImpl implements SrmRestInternalService {
         String dsdId = dataStructureComponents[1];
         String version = dataStructureComponents[2];
         return restApiLocator.getSrmRestInternalFacadeV10().retrieveDataStructure(agencyId, dsdId, version);
+    }
+    
+    @Override
+    public DataStructures findDsds(int firstResult, int maxResult, String condition) {
+        String limit = String.valueOf(maxResult);
+        String offset = String.valueOf(firstResult);
+        return restApiLocator.getSrmRestInternalFacadeV10().findDataStructures(condition, null, limit, offset);
     }
     
 }
