0f63d735f4003bd3f9eff10671e605e7ab399c28
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-05-13 14:20:52 +0000

    Eliminados estados no usados
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@45865 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/model/ds/LifeCycleResourceDS.java
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/model/ds/LifeCycleResourceDS.java
@@ -3,24 +3,20 @@ package org.siemac.metamac.statistical.resources.web.client.model.ds;
 public class LifeCycleResourceDS extends VersionableResourceDS {
 
     // CONTENT DESCRIPTORS
-    public static final String DESCRIPTION                      = "lc-desc";
+    public static final String DESCRIPTION                = "lc-desc";
 
     // LIFE CYCLE
-    public static final String PROC_STATUS                      = "lc-proc-status";
-    public static final String CREATION_DATE                    = "lc-creat-date";
-    public static final String CREATION_USER                    = "lc-creat-user";
-    public static final String PRODUCTION_VALIDATION_DATE       = "lc-prod-val-date";
-    public static final String PRODUCTION_VALIDATION_USER       = "lc-prod-val-user";
-    public static final String DIFFUSION_VALIDATION_DATE        = "lc-diff-val-date";
-    public static final String DIFFUSION_VALIDATION_USER        = "lc-diff-val-user";
-    public static final String REJECT_VALIDATION_DATE           = "lc-rej-val-date";
-    public static final String REJECT_VALIDATION_USER           = "lc-rej-val-user";
-    public static final String INTERNAL_PUBLICATION_DATE        = "lc-int-pub-date";
-    public static final String INTERNAL_PUBLICATION_USER        = "lc-int-pub-user";
-    public static final String EXTERNAL_PUBLICATION_DATE        = "lc-ext-pub-date";
-    public static final String EXTERNAL_PUBLICATION_USER        = "lc-ext-pub-user";
-    public static final String IS_EXTERNAL_PUBLICATION_FAILED   = "lc-is-ext-pub-failed";
-    public static final String EXTERNAL_PUBLICATION_FAILED_DATE = "lc-ext-pub-failed-date";
-    public static final String REPLACES_VERSION                 = "lc-replaces";
-    public static final String IS_REPLACED_BY_VERSION           = "lc-replaced-by";
+    public static final String PROC_STATUS                = "lc-proc-status";
+    public static final String CREATION_DATE              = "lc-creat-date";
+    public static final String CREATION_USER              = "lc-creat-user";
+    public static final String PRODUCTION_VALIDATION_DATE = "lc-prod-val-date";
+    public static final String PRODUCTION_VALIDATION_USER = "lc-prod-val-user";
+    public static final String DIFFUSION_VALIDATION_DATE  = "lc-diff-val-date";
+    public static final String DIFFUSION_VALIDATION_USER  = "lc-diff-val-user";
+    public static final String REJECT_VALIDATION_DATE     = "lc-rej-val-date";
+    public static final String REJECT_VALIDATION_USER     = "lc-rej-val-user";
+    public static final String PUBLICATION_DATE           = "lc-pub-date";
+    public static final String PUBLICATION_USER           = "lc-pub-user";
+    public static final String REPLACES_VERSION           = "lc-replaces";
+    public static final String IS_REPLACED_BY_VERSION     = "lc-replaced-by";
 }
