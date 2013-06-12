122d402ad7dafcff70e930b5446017b5a91c4b7c
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-06-12 12:29:25 +0000

    METAMAC-1564 - Configuración y control de propiedades del DATA para aplicaciones internas y externas >> Añadidas propiedades de web applications
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@46729 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/server/listener/StatisticalResourcesStartupListener.java
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/server/listener/StatisticalResourcesStartupListener.java
@@ -10,33 +10,32 @@ public class StatisticalResourcesStartupListener extends ApplicationStartupListe
     public void checkConfiguration() {
 
         // SECURITY
-
         checkSecurityProperties();
 
         // DATASOURCE
-
         checkRequiredProperty(StatisticalResourcesConfigurationConstants.DB_DRIVER_NAME);
         checkRequiredProperty(StatisticalResourcesConfigurationConstants.DB_URL);
         checkRequiredProperty(StatisticalResourcesConfigurationConstants.DB_USERNAME);
         checkRequiredProperty(StatisticalResourcesConfigurationConstants.DB_PASSWORD);
         checkRequiredProperty(StatisticalResourcesConfigurationConstants.DB_DIALECT);
 
-        // API
+        // WEB APPLICATIONS
+        checkRequiredProperty(ConfigurationConstants.WEB_APPLICATION_STATISTICAL_OPERATIONS_INTERNAL_WEB);
+        checkRequiredProperty(ConfigurationConstants.WEB_APPLICATION_SRM_INTERNAL_WEB);
+        checkRequiredProperty(ConfigurationConstants.WEB_APPLICATION_COMMON_METADATA_INTERNAL_WEB);
 
+        // API
         checkRequiredProperty(ConfigurationConstants.ENDPOINT_SRM_INTERNAL_API);
         checkRequiredProperty(ConfigurationConstants.ENDPOINT_STATISTICAL_OPERATIONS_INTERNAL_API);
         checkRequiredProperty(ConfigurationConstants.ENDPOINT_COMMON_METADATA_EXTERNAL_API);
 
         // OTHER CONFIGURATION PROPERTIES
-
         // Common properties
-
         checkEditionLanguagesProperty();
         checkNavBarUrlProperty();
         checkOrganisationProperty();
 
         // Statistical resources properties
-
         checkRequiredProperty(StatisticalResourcesConfigurationConstants.USER_GUIDE_FILE_NAME);
 
         // TODO add properties to check
