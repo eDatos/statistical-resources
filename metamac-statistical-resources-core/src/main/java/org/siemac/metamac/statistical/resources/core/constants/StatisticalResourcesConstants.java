a0f8eb8f6ea51efc858ed12b7a2690345d773ae9
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-03 11:52:48 +0000

    Estableicendo locale por defecto para guardar y obtener datos del datasetrepository porque NO serán internacionalizable
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@47234 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/constants/StatisticalResourcesConstants.java
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/constants/StatisticalResourcesConstants.java
@@ -3,4 +3,5 @@ package org.siemac.metamac.statistical.resources.core.constants;
 public class StatisticalResourcesConstants {
 
     public static final String SECURITY_APPLICATION_ID = "GESTOR_RECURSOS_ESTADISTICOS";
+    public static final String DEFAULT_DATA_REPOSITORY_LOCALE = "es";
 }
