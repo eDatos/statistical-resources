5169d79c000712f73145dd1aa056174f71011b76
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-03 12:57:02 +0000

    Srm rest internal service con nuevos metodos
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@47239 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/invocation/SrmRestInternalService.java
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/invocation/SrmRestInternalService.java
@@ -1,5 +1,9 @@
 package org.siemac.metamac.statistical.resources.core.invocation;
 
+import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.Codelist;
+import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.Codes;
+import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.Concept;
+import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.ConceptScheme;
 import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.DataStructure;
 import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.DataStructures;
 
@@ -9,4 +13,12 @@ public interface SrmRestInternalService {
     DataStructure retrieveDsdByUrn(String urn);
 
     DataStructures findDsds(int firstResult, int maxResult, String condition);
+
+    Concept retrieveConceptByUrn(String urn);
+
+    Codelist retrieveCodelistByUrn(String urn);
+    
+    Codes findCodes(String codelistUrn, int firstResult, int maxResult, String query);
+
+    ConceptScheme retrieveConceptSchemeByUrn(String urn);
 }
--- metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/invocation/SrmRestInternalServiceImpl.java
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/invocation/SrmRestInternalServiceImpl.java
@@ -1,6 +1,11 @@
 package org.siemac.metamac.statistical.resources.core.invocation;
 
 import org.siemac.metamac.core.common.util.GeneratorUrnUtils;
+import org.siemac.metamac.core.common.util.shared.UrnUtils;
+import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.Codelist;
+import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.Codes;
+import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.Concept;
+import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.ConceptScheme;
 import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.DataStructure;
 import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.DataStructures;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +16,10 @@ public class SrmRestInternalServiceImpl implements SrmRestInternalService {
 
     @Autowired
     private MetamacApisLocator restApiLocator;
+    
+    /*
+     * DSD 
+     */
     
     @Override
     public DataStructure retrieveDsdByUrn(String urn) {
@@ -26,6 +35,58 @@ public class SrmRestInternalServiceImpl implements SrmRestInternalService {
         String limit = String.valueOf(maxResult);
         String offset = String.valueOf(firstResult);
         return restApiLocator.getSrmRestInternalFacadeV10().findDataStructures(condition, null, limit, offset);
+    }
+    
+    /*
+     * Concept schemes
+     */
+    @Override
+    public ConceptScheme retrieveConceptSchemeByUrn(String urn) {
+        String[] params = UrnUtils.splitUrnItemScheme(urn);
+        String agencyId = params[0];
+        String resourceId = params[1];
+        String version = params[2];
+        return restApiLocator.getSrmRestInternalFacadeV10().retrieveConceptScheme(agencyId, resourceId, version);
+    }
+    
+    /*
+     * Concepts
+     */
+    
+    @Override
+    public Concept retrieveConceptByUrn(String urn) {
+        String[] params = UrnUtils.splitUrnItem(urn);
+        String agencyId = params[0];
+        String schemeId = params[1];
+        String version = params[2];
+        String conceptId = params[3];
+        return restApiLocator.getSrmRestInternalFacadeV10().retrieveConcept(agencyId, schemeId, version, conceptId);
+    }
+    
+    /*
+     * Code lists
+     */
+    @Override
+    public Codelist retrieveCodelistByUrn(String urn) {
+        String[] params = UrnUtils.splitUrnItemScheme(urn);
+        String agencyId = params[0];
+        String resourceId = params[1];
+        String version = params[2];
+        return restApiLocator.getSrmRestInternalFacadeV10().retrieveCodelist(agencyId, resourceId, version);
+    }
+    
+    /*
+     * Codes
+     */
+    @Override
+    public Codes findCodes(String codelistUrn, int firstResult, int maxResult, String query) {
+        String limit = String.valueOf(maxResult);
+        String offset = String.valueOf(firstResult);
+        String[] params = UrnUtils.splitUrnItemScheme(codelistUrn);
+        String agencyId = params[0];
+        String resourceId = params[1];
+        String version = params[2];
+        return restApiLocator.getSrmRestInternalFacadeV10().findCodes(agencyId, resourceId, version, query, null, limit, offset);
     }
     
 }
