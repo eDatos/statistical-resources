174b39734c494952392313ca7ee3f7227e552dac
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-09 12:10:54 +0000

    METAMAC-1570: [API] Implementación de la API
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@47386 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-rest-external-api/src/main/java/org/siemac/metamac/statistical_resources/rest/external/RestExternalConstants.java
+++ metamac-statistical-resources-rest-external-api/src/main/java/org/siemac/metamac/statistical_resources/rest/external/RestExternalConstants.java
@@ -2,28 +2,7 @@ package org.siemac.metamac.statistical_resources.rest.external;
 
 public class RestExternalConstants {
 
-    public static String KIND_SEPARATOR                     = "#";
-    public static String WILDCARD                           = "~all";
-/*
- * TODO definir links, kind...
-    public static String LINK_SUBPATH_OPERATIONS            = "operations";
-    public static String LINK_SUBPATH_FAMILIES              = "families";
-    public static String LINK_SUBPATH_INSTANCES             = "instances";
+    public static String KIND_SEPARATOR = "#";
+    public static String WILDCARD       = "~all";
 
-    public static String API_NAME                           = "statisticalOperations";
-    public static String API_VERSION_1_0                    = "v1.0";
-
-    public static String KIND_OPERATIONS                    = API_NAME + KIND_SEPARATOR + "operations";
-    public static String KIND_OPERATION                     = API_NAME + KIND_SEPARATOR + "operation";
-    public static String KIND_FAMILIES                      = API_NAME + KIND_SEPARATOR + "families";
-    public static String KIND_FAMILY                        = API_NAME + KIND_SEPARATOR + "family";
-    public static String KIND_INSTANCE                      = API_NAME + KIND_SEPARATOR + "instance";
-    public static String KIND_INSTANCES                     = API_NAME + KIND_SEPARATOR + "instances";
-    public static String KIND_STATISTICAL_OPERATION_TYPES   = API_NAME + KIND_SEPARATOR + "statisticalOperationTypes";
-    public static String KIND_OFFICIALITY_TYPES             = API_NAME + KIND_SEPARATOR + "officialityTypes";
-    public static String KIND_INSTANCE_TYPES                = API_NAME + KIND_SEPARATOR + "instanceTypes";
-    public static String KIND_STATISTICAL_OPERATION_SOURCES = API_NAME + KIND_SEPARATOR + "statisticalOperationSources";
-    public static String KIND_COLL_METHODS                  = API_NAME + KIND_SEPARATOR + "collMethods";
-    public static String KIND_COSTS                         = API_NAME + KIND_SEPARATOR + "costs";
-    */
 }
--- metamac-statistical-resources-rest-external-api/src/main/java/org/siemac/metamac/statistical_resources/rest/external/v1_0/service/StatisticalResourcesV1_0.java
+++ metamac-statistical-resources-rest-external-api/src/main/java/org/siemac/metamac/statistical_resources/rest/external/v1_0/service/StatisticalResourcesV1_0.java
@@ -4,8 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 
+import org.siemac.metamac.rest.statistical_resources.v1_0.domain.Dataset;
 
 @Path("v1.0")
 // IMPORTANT: If a new version of API is added, remember change latest url y urlrewrite.xml in war
@@ -13,7 +13,7 @@ public interface StatisticalResourcesV1_0 {
 
     @GET
     @Produces("application/xml")
-    @Path("xxx/{id}")
-    Dataset retrieveDataset(@PathParam("id") String id);
+    @Path("datasets/{agencyID}/{resourceID}/{version}")
+    Dataset retrieveDataset(@PathParam("agencyID") String agencyID, @PathParam("resourceID") String resourceID, @PathParam("version") String version);
 
 }
