8e0bc5d29605d417f14d244c1d18187bac6c949d
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-09 13:44:57 +0000

    METAMAC-1564 Listener para comprobar propiedades de configuración del data
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@47398 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/constants/StatisticalResourcesConfigurationConstants.java
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/constants/StatisticalResourcesConfigurationConstants.java
@@ -8,15 +8,22 @@ public class StatisticalResourcesConfigurationConstants extends ConfigurationCon
 
     // Configuration
 
-    public static final String USER_GUIDE_FILE_NAME = "metamac.statistical.resources.user.guide.file.name";
+    public static final String USER_GUIDE_FILE_NAME                = "metamac.statistical.resources.user.guide.file.name";
     public static final String GEOGRAPHIC_GRANULARITY_CODELIST_URN = "metamac.statistical.resources.geographic_granularity.codelist.urn";
-    public static final String TEMPORAL_GRANULARITY_CODELIST_URN = "metamac.statistical.resources.temporal_granularity.codelist.urn";
-    
-    // DataSource
+    public static final String TEMPORAL_GRANULARITY_CODELIST_URN   = "metamac.statistical.resources.temporal_granularity.codelist.urn";
 
-    public static final String DB_URL               = "metamac.statistical.resources.db.url";
-    public static final String DB_USERNAME          = "metamac.statistical.resources.db.username";
-    public static final String DB_PASSWORD          = "metamac.statistical.resources.db.password";
-    public static final String DB_DIALECT           = "metamac.statistical.resources.db.dialect";
-    public static final String DB_DRIVER_NAME       = "metamac.statistical.resources.db.driverName";
+    // DataSources
+
+    public static final String DB_URL                              = "metamac.statistical.resources.db.url";
+    public static final String DB_USERNAME                         = "metamac.statistical.resources.db.username";
+    public static final String DB_PASSWORD                         = "metamac.statistical.resources.db.password";
+    public static final String DB_DIALECT                          = "metamac.statistical.resources.db.dialect";
+    public static final String DB_DRIVER_NAME                      = "metamac.statistical.resources.db.driverName";
+
+    public static final String DB_REPOSITORY_URL                   = "metamac.statistical.resources.repo.db.url";
+    public static final String DB_REPOSITORY_USERNAME              = "metamac.statistical.resources.repo.db.username";
+    public static final String DB_REPOSITORY_PASSWORD              = "metamac.statistical.resources.repo.db.password";
+    public static final String DB_REPOSITORY_DIALECT               = "metamac.statistical.resources.repo.db.dialect";
+    public static final String DB_REPOSITORY_DRIVER_NAME           = "metamac.statistical.resources.repo.db.dialect";
+
 }
