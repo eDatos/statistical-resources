4f109a6fd039cf9ca9a80fe590cf2bb48462f321
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-09 12:29:45 +0000

    METAMAC-1570: [API] Implementación de la API
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@47389 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-rest-external-impl/src/main/java/org/siemac/metamac/statistical_resources/rest/external/v1_0/service/StatisticalResourcesRestExternalFacadeV10Impl.java
+++ metamac-statistical-resources-rest-external-impl/src/main/java/org/siemac/metamac/statistical_resources/rest/external/v1_0/service/StatisticalResourcesRestExternalFacadeV10Impl.java
@@ -1,44 +1,33 @@
 package org.siemac.metamac.statistical_resources.rest.external.v1_0.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.ws.rs.core.Response.Status;
 
-import org.fornax.cartridges.sculptor.framework.accessapi.ConditionalCriteria;
-import org.fornax.cartridges.sculptor.framework.accessapi.ConditionalCriteriaBuilder;
-import org.fornax.cartridges.sculptor.framework.domain.PagedResult;
-import org.fornax.cartridges.sculptor.framework.domain.PagingParameter;
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
-import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.rest.exception.RestCommonServiceExceptionType;
 import org.siemac.metamac.rest.exception.RestException;
 import org.siemac.metamac.rest.exception.utils.RestExceptionUtils;
-import org.siemac.metamac.rest.search.criteria.SculptorCriteria;
+import org.siemac.metamac.rest.statistical_resources.v1_0.domain.Dataset;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("statisticalResourcesRestExternalFacadeV10")
 public class StatisticalResourcesRestExternalFacadeV10Impl implements StatisticalResourcesV1_0 {
 
-    private final ServiceContext                serviceContextRestExternal = new ServiceContext("restExternal", "restExternal", "restExternal");
-    private final Logger                        logger                     = LoggerFactory.getLogger(StatisticalResourcesRestExternalFacadeV10Impl.class);
+    private final ServiceContext serviceContextRestExternal = new ServiceContext("restExternal", "restExternal", "restExternal");
+    private final Logger         logger                     = LoggerFactory.getLogger(StatisticalResourcesRestExternalFacadeV10Impl.class);
 
     @Override
-    public Dataset retrieveDataset(String id) {
+    public Dataset retrieveDataset(String agencyID, String resourceID, String version) {
         try {
             // Retrieve
 
             // Transform
             return null;
-
         } catch (Exception e) {
             throw manageException(e);
         }
     }
-
 
     /**
      * Throws response error, logging exception
