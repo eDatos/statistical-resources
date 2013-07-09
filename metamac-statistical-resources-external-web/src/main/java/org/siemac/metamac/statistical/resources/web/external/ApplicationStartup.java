3f1c105cb3ac099aba01a0f758b732a96b9b89ba
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-09 12:39:12 +0000

    METAMAC-1570: [API] Implementación de la API. Aplicación web externa
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@47392 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-statistical-resources-external-web/src/main/java/org/siemac/metamac/statistical/resources/web/external/ApplicationStartup.java
@@ -0,0 +1,55 @@
+package org.siemac.metamac.statistical.resources.web.external;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.siemac.metamac.core.common.conf.ConfigurationService;
+import org.siemac.metamac.core.common.util.ApplicationContextProvider;
+import org.siemac.metamac.statistical.operations.core.constants.StatisticalResourcesConfigurationConstants;
+
+public class ApplicationStartup implements ServletContextListener {
+
+    private static final Log     LOG = LogFactory.getLog(ApplicationStartup.class);
+
+    private ConfigurationService configurationService;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            configurationService = ApplicationContextProvider.getApplicationContext().getBean(ConfigurationService.class);
+            checkConfiguration();
+        } catch (Exception e) {
+            // Abort startup application
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void checkConfiguration() {
+        LOG.info("**********************************************************");
+        LOG.info("Checking application configuration");
+        LOG.info("**********************************************************");
+
+        // Datasource
+        configurationService.checkRequiredProperty(StatisticalResourcesConfigurationConstants.DB_DRIVER_NAME);
+        configurationService.checkRequiredProperty(StatisticalResourcesConfigurationConstants.DB_URL);
+        configurationService.checkRequiredProperty(StatisticalResourcesConfigurationConstants.DB_USERNAME);
+        configurationService.checkRequiredProperty(StatisticalResourcesConfigurationConstants.DB_PASSWORD);
+        configurationService.checkRequiredProperty(StatisticalResourcesConfigurationConstants.DB_DIALECT);
+
+        // Api
+        configurationService.checkRequiredProperty(StatisticalResourcesConfigurationConstants.ENDPOINT_STATISTICAL_OPERATIONS_EXTERNAL_API);
+        configurationService.checkRequiredProperty(StatisticalResourcesConfigurationConstants.ENDPOINT_COMMON_METADATA_EXTERNAL_API);
+        configurationService.checkRequiredProperty(StatisticalResourcesConfigurationConstants.ENDPOINT_SRM_EXTERNAL_API);
+
+        LOG.info("**********************************************************");
+        LOG.info("Application configuration checked");
+        LOG.info("**********************************************************");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+
+}
