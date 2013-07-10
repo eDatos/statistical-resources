82f2862f7d2e5d82d361aa6dff58d55f7f8fb658
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-10 06:24:06 +0000

    METAMAC-1769: Importación de datos de dataset http://jira.arte-consultores.com/browse/METAMAC-1769
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@47403 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-core/src/test/java/org/siemac/metamac/statistical/resources/core/task/serviceapi/TaskServiceFacadeTest.java
+++ metamac-statistical-resources-core/src/test/java/org/siemac/metamac/statistical/resources/core/task/serviceapi/TaskServiceFacadeTest.java
@@ -1,28 +1,22 @@
 package org.siemac.metamac.statistical.resources.core.task.serviceapi;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.siemac.metamac.statistical.resources.core.StatisticalResourcesBaseTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Spring based transactional test with DbUnit support.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations = {"classpath:spring/statistical-resources/applicationContext-test.xml"})
-//@TransactionConfiguration(transactionManager = "txManager", defaultRollback = false)
-//@Transactional
+// @RunWith(SpringJUnit4ClassRunner.class)
+// @ContextConfiguration(locations = {"classpath:spring/statistical-resources/applicationContext-test.xml"})
+// @TransactionConfiguration(transactionManager = "txManager", defaultRollback = false)
+// @Transactional
 public class TaskServiceFacadeTest extends StatisticalResourcesBaseTest implements TaskServiceFacadeTestBase {
 
     @Autowired
     protected TaskServiceFacade taskServiceFacade;
 
     @Override
-    @Test
+    // @Test
     public void testExecuteImportationTask() throws Exception {
         // TODO Auto-generated method stub
     }
--- metamac-statistical-resources-core/src/test/java/org/siemac/metamac/statistical/resources/core/task/serviceapi/TaskServiceTest.java
+++ metamac-statistical-resources-core/src/test/java/org/siemac/metamac/statistical/resources/core/task/serviceapi/TaskServiceTest.java
@@ -1,31 +1,28 @@
 package org.siemac.metamac.statistical.resources.core.task.serviceapi;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.siemac.metamac.statistical.resources.core.StatisticalResourcesBaseTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Spring based transactional test with DbUnit support.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations = {"classpath:spring/statistical-resources/applicationContext-test.xml"})
-//@TransactionConfiguration(transactionManager = "txManager", defaultRollback = false)
-//@Transactional
+// @RunWith(SpringJUnit4ClassRunner.class)
+// @ContextConfiguration(locations = {"classpath:spring/statistical-resources/applicationContext-test.xml"})
+// @TransactionConfiguration(transactionManager = "txManager", defaultRollback = false)
+// @Transactional
 public class TaskServiceTest extends StatisticalResourcesBaseTest implements TaskServiceTestBase {
 
     @Autowired
     protected TaskService taskService;
 
     @Override
-    @Test
+    // @Test
     public void testPlannifyImportationDataset() throws Exception {
         // TODO Auto-generated method stub
     }
 
     @Override
-    @Test
+    // @Test
     public void testProcessImportationTask() throws Exception {
         // TODO Auto-generated method stub
     }
