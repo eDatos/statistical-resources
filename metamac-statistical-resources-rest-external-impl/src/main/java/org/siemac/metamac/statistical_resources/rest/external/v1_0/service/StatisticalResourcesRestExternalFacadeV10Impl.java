121f4b05b7a78a01fddf4fa4e6995ffcf6a92042
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-10 09:14:15 +0000

    METAMAC-1570: [API] Implementación de la API. Devolver la respuesta en JSON y XML
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@47423 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-rest-external-impl/src/main/java/org/siemac/metamac/statistical_resources/rest/external/v1_0/service/StatisticalResourcesRestExternalFacadeV10Impl.java
+++ metamac-statistical-resources-rest-external-impl/src/main/java/org/siemac/metamac/statistical_resources/rest/external/v1_0/service/StatisticalResourcesRestExternalFacadeV10Impl.java
@@ -3,6 +3,8 @@ package org.siemac.metamac.statistical_resources.rest.external.v1_0.service;
 import javax.ws.rs.core.Response.Status;
 
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
+import org.siemac.metamac.rest.common.v1_0.domain.InternationalString;
+import org.siemac.metamac.rest.common.v1_0.domain.LocalisedString;
 import org.siemac.metamac.rest.exception.RestCommonServiceExceptionType;
 import org.siemac.metamac.rest.exception.RestException;
 import org.siemac.metamac.rest.exception.utils.RestExceptionUtils;
@@ -20,15 +22,29 @@ public class StatisticalResourcesRestExternalFacadeV10Impl implements Statistica
     @Override
     public Dataset retrieveDataset(String agencyID, String resourceID, String version) {
         try {
+            // TODO retrieveDataset
             // Retrieve
+            Dataset dataset = new Dataset();
+            dataset.setId(resourceID);
+            dataset.setKind("aa");
+            InternationalString name = new InternationalString();
+            LocalisedString es = new LocalisedString();
+            es.setLang("es");
+            es.setValue("nombre español");
+            name.getTexts().add(es);
+            LocalisedString en = new LocalisedString();
+            en.setLang("en");
+            en.setValue("name english");
+            name.getTexts().add(en);
+            dataset.setName(name);
 
+            dataset.setUrn("urn:" + resourceID);
             // Transform
-            return null;
+            return dataset;
         } catch (Exception e) {
             throw manageException(e);
         }
     }
-
     /**
      * Throws response error, logging exception
      */
