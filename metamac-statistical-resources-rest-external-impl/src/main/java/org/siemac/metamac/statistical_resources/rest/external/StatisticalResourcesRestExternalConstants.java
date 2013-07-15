2f9a538dfac7d9833725765c40ce2621c3d71928
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-15 13:13:16 +0000

    In progress: METAMAC-1783: [API] Obtener un dataset. DATA
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@47561 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-rest-external-impl/src/main/java/org/siemac/metamac/statistical_resources/rest/external/StatisticalResourcesRestExternalConstants.java
+++ metamac-statistical-resources-rest-external-impl/src/main/java/org/siemac/metamac/statistical_resources/rest/external/StatisticalResourcesRestExternalConstants.java
@@ -2,8 +2,9 @@ package org.siemac.metamac.statistical_resources.rest.external;
 
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
 
-public class StatisticalResourcesRestExternalConstants {
+// TODO refactor clase
+public class StatisticalResourcesRestExternalConstants extends RestExternalConstants {
 
-    public static final ServiceContext ctx = new ServiceContext("restExternal", "restExternal", "restExternal");
+    public static final ServiceContext SERVICE_CONTEXT = new ServiceContext("restExternal", "restExternal", "restExternal");
 
 }
