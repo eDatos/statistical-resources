cb85360ed86fc920c7a29f1e5888ed28069cb98b
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-18 08:38:03 +0000

    Criterias nuevos
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@47657 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/shared/criteria/base/HasSchemeCriteria.java
@@ -0,0 +1,8 @@
+package org.siemac.metamac.statistical.resources.web.shared.criteria.base;
+
+public interface HasSchemeCriteria {
+
+    String getSchemeUrn();
+
+    void setSchemeUrn(String schemeUrn);
+}
---
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/shared/criteria/base/HasStatisticalOperationCriteria.java
@@ -0,0 +1,9 @@
+package org.siemac.metamac.statistical.resources.web.shared.criteria.base;
+
+public interface HasStatisticalOperationCriteria {
+
+    public String getStatisticalOperationUrn();
+
+    public void setStatisticalOperationUrn(String statisticalOperationUrn);
+
+}
