a18842cfc5298e14c8ca5c121ba916b395ba7f12
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-26 07:33:56 +0000

    Statistialresource web criteria
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@48103 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/shared/criteria/StatisticalResourceWebCriteria.java
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/shared/criteria/StatisticalResourceWebCriteria.java
@@ -1,8 +1,9 @@
 package org.siemac.metamac.statistical.resources.web.shared.criteria;
 
+import org.siemac.metamac.statistical.resources.web.shared.criteria.base.HasStatisticalOperationCriteria;
 import org.siemac.metamac.web.common.shared.criteria.MetamacWebCriteria;
 
-public class StatisticalResourceWebCriteria extends MetamacWebCriteria {
+public class StatisticalResourceWebCriteria extends MetamacWebCriteria implements HasStatisticalOperationCriteria {
 
     private static final long serialVersionUID = 1L;
 
@@ -15,10 +16,12 @@ public class StatisticalResourceWebCriteria extends MetamacWebCriteria {
         super(criteria);
     }
 
+    @Override
     public String getStatisticalOperationUrn() {
         return statisticalOperationUrn;
     }
 
+    @Override
     public void setStatisticalOperationUrn(String statisticalOperationUrn) {
         this.statisticalOperationUrn = statisticalOperationUrn;
     }
