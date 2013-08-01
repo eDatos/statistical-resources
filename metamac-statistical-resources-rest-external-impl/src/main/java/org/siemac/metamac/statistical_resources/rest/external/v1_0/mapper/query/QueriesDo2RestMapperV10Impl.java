459fada57e88aef7991937bb0b4cfca151a8788d
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-08-01 14:15:58 +0000

    in progress: METAMAC-1801 [API] Obtener query
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@48316 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-rest-external-impl/src/main/java/org/siemac/metamac/statistical_resources/rest/external/v1_0/mapper/query/QueriesDo2RestMapperV10Impl.java
+++ metamac-statistical-resources-rest-external-impl/src/main/java/org/siemac/metamac/statistical_resources/rest/external/v1_0/mapper/query/QueriesDo2RestMapperV10Impl.java
@@ -2,13 +2,16 @@ package org.siemac.metamac.statistical_resources.rest.external.v1_0.mapper.query
 
 import java.util.List;
 
+import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.rest.common.v1_0.domain.ChildLinks;
 import org.siemac.metamac.rest.common.v1_0.domain.Resource;
 import org.siemac.metamac.rest.common.v1_0.domain.ResourceLink;
 import org.siemac.metamac.rest.statistical_resources.v1_0.domain.Query;
+import org.siemac.metamac.rest.statistical_resources.v1_0.domain.QueryMetadata;
 import org.siemac.metamac.statistical.resources.core.query.domain.QueryVersion;
 import org.siemac.metamac.statistical_resources.rest.external.RestExternalConstants;
 import org.siemac.metamac.statistical_resources.rest.external.v1_0.mapper.base.CommonDo2RestMapperV10;
+import org.siemac.metamac.statistical_resources.rest.external.v1_0.mapper.dataset.DatasetsDo2RestMapperV10;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,7 +19,10 @@ import org.springframework.stereotype.Component;
 public class QueriesDo2RestMapperV10Impl implements QueriesDo2RestMapperV10 {
 
     @Autowired
-    private CommonDo2RestMapperV10 commonDo2RestMapper;
+    private CommonDo2RestMapperV10   commonDo2RestMapper;
+
+    @Autowired
+    private DatasetsDo2RestMapperV10 datasetsDo2RestMapper;
 
     @Override
     public Query toQuery(QueryVersion source, List<String> selectedLanguages, boolean includeMetadata, boolean includeData) throws Exception {
@@ -35,11 +41,11 @@ public class QueriesDo2RestMapperV10Impl implements QueriesDo2RestMapperV10 {
         target.setParentLink(toQueryParentLink(source));
         target.setChildLinks(toQueryChildLinks(source));
         target.setSelectedLanguages(commonDo2RestMapper.toLanguages(selectedLanguages));
-        // TODO metadata
-        // if (includeMetadata) {
-        // target.setMetadata(toQueryMetadata(source, selectedLanguages));
-        // }
+        if (includeMetadata) {
+            target.setMetadata(toQueryMetadata(source, selectedLanguages));
+        }
         // TODO DATA
+        // TODO selection?
         return target;
     }
 
@@ -57,6 +63,23 @@ public class QueriesDo2RestMapperV10Impl implements QueriesDo2RestMapperV10 {
         return target;
     }
 
+    private QueryMetadata toQueryMetadata(QueryVersion source, List<String> selectedLanguages) throws MetamacException {
+        if (source == null) {
+            return null;
+        }
+        QueryMetadata target = new QueryMetadata();
+
+        target.setRelatedDataset(datasetsDo2RestMapper.toResource(source.getDatasetVersion(), selectedLanguages));
+        target.setStatisticalOperation(commonDo2RestMapper.toResourceExternalItemStatisticalOperations(source.getLifeCycleStatisticalResource().getStatisticalOperation(), selectedLanguages));
+        target.setPublicationDate(commonDo2RestMapper.toDate(source.getLifeCycleStatisticalResource().getPublicationDate()));
+        target.setMaintainer(commonDo2RestMapper.toResourceExternalItemSrm(source.getLifeCycleStatisticalResource().getMaintainer(), selectedLanguages));
+        target.setValidFrom(commonDo2RestMapper.toDate(source.getLifeCycleStatisticalResource().getValidFrom()));
+        target.setValidTo(commonDo2RestMapper.toDate(source.getLifeCycleStatisticalResource().getValidTo()));
+        //
+        // TODO status
+        // TODO type
+        return target;
+    }
     private ResourceLink toQueryParentLink(QueryVersion source) {
         return toQueriesSelfLink(null, null);
     }
