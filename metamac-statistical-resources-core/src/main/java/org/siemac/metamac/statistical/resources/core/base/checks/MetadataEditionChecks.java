48a052a5afe64a001484d7c92ab5d8bc88572543
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-08-01 07:56:11 +0000

    METAMAC-1558 Metadato versionRationaleType
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@48277 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/base/checks/MetadataEditionChecks.java
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/base/checks/MetadataEditionChecks.java
@@ -2,7 +2,7 @@ package org.siemac.metamac.statistical.resources.core.base.checks;
 
 import org.siemac.metamac.statistical.resources.core.enume.domain.NextVersionTypeEnum;
 import org.siemac.metamac.statistical.resources.core.enume.domain.ProcStatusEnum;
-
+import org.siemac.metamac.statistical.resources.core.utils.StatisticalResourcesVersionUtils;
 
 public class MetadataEditionChecks {
 
@@ -12,14 +12,14 @@ public class MetadataEditionChecks {
         }
         return false;
     }
-    
+
     public static boolean canMaintainerBeEdited(Long id) {
         if (id == null) {
             return true;
         }
         return false;
     }
-    
+
     public static boolean canKeywordsBeEdited(ProcStatusEnum procStatus) {
         return true;
     }
@@ -29,5 +29,12 @@ public class MetadataEditionChecks {
             return true;
         }
         return false;
+    }
+
+    public static boolean canVersionRationaleTypesBeEdited(String versionLogic) {
+        if (StatisticalResourcesVersionUtils.INITIAL_VERSION.equals(versionLogic)) {
+            return false;
+        }
+        return true;
     }
 }
