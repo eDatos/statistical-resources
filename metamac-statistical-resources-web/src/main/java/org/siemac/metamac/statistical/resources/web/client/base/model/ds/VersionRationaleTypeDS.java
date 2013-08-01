7fdad8e10df135a5acf30038a39316f55a2f78cb
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-08-01 06:51:27 +0000

    METAMAC-1558 Metadato versionRationaleType
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@48270 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/base/model/ds/VersionRationaleTypeDS.java
@@ -0,0 +1,10 @@
+package org.siemac.metamac.statistical.resources.web.client.base.model.ds;
+
+import com.smartgwt.client.data.DataSource;
+
+public class VersionRationaleTypeDS extends DataSource {
+
+    public static final String VALUE = "ver-value";
+
+    public static final String DTO   = "ver-dto";
+}
---
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/base/model/record/VersionRationaleTypeRecord.java
@@ -0,0 +1,29 @@
+package org.siemac.metamac.statistical.resources.web.client.base.model.record;
+
+import org.siemac.metamac.statistical.resources.core.dto.VersionRationaleTypeDto;
+import org.siemac.metamac.statistical.resources.web.client.base.model.ds.VersionRationaleTypeDS;
+
+import com.smartgwt.client.widgets.grid.ListGridRecord;
+
+public class VersionRationaleTypeRecord extends ListGridRecord {
+
+    public VersionRationaleTypeRecord() {
+
+    }
+
+    public void setValue(String value) {
+        setAttribute(VersionRationaleTypeDS.VALUE, value);
+    }
+
+    public String getValue() {
+        return getAttributeAsString(VersionRationaleTypeDS.VALUE);
+    }
+
+    public void setVersionRationaleTypeDto(VersionRationaleTypeDto versionRationaleTypeDto) {
+        setAttribute(VersionRationaleTypeDS.DTO, versionRationaleTypeDto);
+    }
+
+    public VersionRationaleTypeDto getVersionRationaleTypeDto() {
+        return (VersionRationaleTypeDto) getAttributeAsObject(VersionRationaleTypeDS.DTO);
+    }
+}
