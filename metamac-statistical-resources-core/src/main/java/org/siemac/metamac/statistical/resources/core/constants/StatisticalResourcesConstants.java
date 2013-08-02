ac6769c4c79917ab422f28b045dc5401cdd17cfd
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-08-02 13:29:39 +0000

    Constante para id de dimensión temporal
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@48373 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/constants/StatisticalResourcesConstants.java
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/constants/StatisticalResourcesConstants.java
@@ -2,7 +2,8 @@ package org.siemac.metamac.statistical.resources.core.constants;
 
 public class StatisticalResourcesConstants {
 
-    public static final String SECURITY_APPLICATION_ID = "GESTOR_RECURSOS_ESTADISTICOS";
-    public static final String DEFAULT_DATA_REPOSITORY_LOCALE = "es";
-    public static final String BIBLIOGRAPHIC_CITATION_URI_TOKEN= "#URI#";
+    public static final String SECURITY_APPLICATION_ID          = "GESTOR_RECURSOS_ESTADISTICOS";
+    public static final String DEFAULT_DATA_REPOSITORY_LOCALE   = "es";
+    public static final String BIBLIOGRAPHIC_CITATION_URI_TOKEN = "#URI#";
+    public static final String TEMPORAL_DIMENSION_ID            = "TIME_PERIOD";
 }
