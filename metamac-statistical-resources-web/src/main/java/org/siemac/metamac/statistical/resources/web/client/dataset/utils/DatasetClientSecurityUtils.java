8d6c8079c872011051b1bb1a6bde15b02bcb88dc
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-08-02 08:47:21 +0000

    METAMAC-1845 Seguridad
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@48334 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/dataset/utils/DatasetClientSecurityUtils.java
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/dataset/utils/DatasetClientSecurityUtils.java
@@ -1,6 +1,10 @@
 package org.siemac.metamac.statistical.resources.web.client.dataset.utils;
 
+import org.siemac.metamac.statistical.resources.core.dto.datasets.DatasetVersionDto;
+import org.siemac.metamac.statistical.resources.core.enume.domain.ProcStatusEnum;
+
 // TODO: Add real rules not only "true"
+// TODO take into account the metadata isTaskInBackground to avoid to execute some actions!
 public class DatasetClientSecurityUtils {
 
     public static boolean canCreateDataset() {
@@ -15,11 +19,20 @@ public class DatasetClientSecurityUtils {
         return true;
     }
 
-    public static boolean canCreateDatasource() {
-        return true;
-    }
-    public static boolean canDeleteDatasource() {
+    public static boolean canImportDatasources(DatasetVersionDto datasetVersionDto) {
+        if (!ProcStatusEnum.DRAFT.equals(datasetVersionDto.getProcStatus())) {
+            // Datasources can only be imported in datasets in DRAFT
+            return false;
+        }
         return true;
     }
 
+    // this method will be called by the button that import datasources in many datasets
+    public static boolean canImportDatasources() {
+        return true;
+    }
+
+    public static boolean canDeleteDatasource() {
+        return true;
+    }
 }
