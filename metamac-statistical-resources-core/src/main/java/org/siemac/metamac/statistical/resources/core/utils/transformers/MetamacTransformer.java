5b5c2d711abfbbcab0147f7f1ea167f0f7c81a39
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-08-06 10:02:14 +0000

    Nuevo transformer
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@48425 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/utils/transformers/MetamacTransformer.java
@@ -0,0 +1,14 @@
+package org.siemac.metamac.statistical.resources.core.utils.transformers;
+
+import org.apache.commons.collections.Transformer;
+
+
+public abstract class MetamacTransformer<T extends Object, R extends Object> implements Transformer {
+
+    @Override
+    public final Object transform(Object input) {
+        return transformItem((T)input);
+    }
+    
+    public abstract R transformItem(T item);
+}
