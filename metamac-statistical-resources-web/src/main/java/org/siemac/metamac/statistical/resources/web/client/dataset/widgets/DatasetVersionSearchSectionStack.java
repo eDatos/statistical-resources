e0e3a909f9b78fef1456b46ad5937ffc4e2818e1
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-08-14 11:07:43 +0000

    METAMAC-1847 [WEB] Búsqueda avanzada: datasets
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@48670 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/dataset/widgets/DatasetVersionSearchSectionStack.java
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/dataset/widgets/DatasetVersionSearchSectionStack.java
@@ -1,9 +1,13 @@
 package org.siemac.metamac.statistical.resources.web.client.dataset.widgets;
 
+import static org.siemac.metamac.statistical.resources.web.client.StatisticalResourcesWeb.getConstants;
+
 import org.siemac.metamac.statistical.resources.web.client.constants.StatisticalResourceWebConstants;
+import org.siemac.metamac.statistical.resources.web.client.dataset.model.ds.DatasetDS;
 import org.siemac.metamac.statistical.resources.web.client.dataset.view.handlers.DatasetListUiHandlers;
 import org.siemac.metamac.statistical.resources.web.client.widgets.SiemacMetadataResourceSearchSectionStack;
 import org.siemac.metamac.statistical.resources.web.shared.criteria.DatasetVersionWebCriteria;
+import org.siemac.metamac.web.common.client.widgets.form.fields.CustomDateItem;
 
 public class DatasetVersionSearchSectionStack extends SiemacMetadataResourceSearchSectionStack {
 
@@ -12,8 +16,26 @@ public class DatasetVersionSearchSectionStack extends SiemacMetadataResourceSear
     public DatasetVersionSearchSectionStack() {
     }
 
+    @Override
+    protected void createAdvancedSearchForm() {
+        super.createAdvancedSearchForm();
+
+        // TODO geographical granularity
+        // TODO temporal granularity
+        CustomDateItem dateStart = new CustomDateItem(DatasetDS.DATE_START, getConstants().datasetDateStart());
+        CustomDateItem dateEnd = new CustomDateItem(DatasetDS.DATE_END, getConstants().datasetDateEnd());
+        // TODO related DSD
+        CustomDateItem dateNextUpdate = new CustomDateItem(DatasetDS.DATE_NEXT_UPDATE, getConstants().datasetDateNextUpdate());
+
+        advancedSearchForm.addFieldsInThePenultimePosition(dateStart, dateEnd, dateNextUpdate);
+    }
+
     public DatasetVersionWebCriteria getDatasetVersionWebCriteria() {
-        return (DatasetVersionWebCriteria) getSiemacMetadataStatisticalResourceWebCriteria(new DatasetVersionWebCriteria());
+        DatasetVersionWebCriteria criteria = (DatasetVersionWebCriteria) getSiemacMetadataStatisticalResourceWebCriteria(new DatasetVersionWebCriteria());
+        criteria.setDateStart(((CustomDateItem) advancedSearchForm.getItem(DatasetDS.DATE_START)).getValueAsDate());
+        criteria.setDateEnd(((CustomDateItem) advancedSearchForm.getItem(DatasetDS.DATE_END)).getValueAsDate());
+        criteria.setDateNextUpdate(((CustomDateItem) advancedSearchForm.getItem(DatasetDS.DATE_NEXT_UPDATE)).getValueAsDate());
+        return criteria;
     }
 
     @Override
