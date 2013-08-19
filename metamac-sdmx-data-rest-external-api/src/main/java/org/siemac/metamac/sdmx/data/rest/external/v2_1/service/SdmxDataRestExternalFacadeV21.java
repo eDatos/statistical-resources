76e97e841c7e58721269a3eee97ff424425342bb
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-08-19 13:17:58 +0000

    LLCC-114: Crear mapper, servicios y callbacks para exportar mensajes SDMX de datos http://jira.arte-consultores.com/browse/LLCC-114
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@48747 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-sdmx-data-rest-external-api/src/main/java/org/siemac/metamac/sdmx/data/rest/external/v2_1/service/SdmxDataRestExternalFacadeV21.java
+++ metamac-sdmx-data-rest-external-api/src/main/java/org/siemac/metamac/sdmx/data/rest/external/v2_1/service/SdmxDataRestExternalFacadeV21.java
@@ -21,4 +21,23 @@ public interface SdmxDataRestExternalFacadeV21 {
     @Path("data/{flowRef}")
     Response findData(@PathParam("flowRef") String flowRef, @DefaultValue("full") @QueryParam("detail") String detail);
 
+    /**
+     * Find data
+     * 
+     * @return Response
+     */
+    @GET
+    @Produces("application/xml")
+    @Path("data/{flowRef}/{key}")
+    Response findData(@PathParam("flowRef") String flowRef, @PathParam("key") String key, @DefaultValue("full") @QueryParam("detail") String detail);
+
+    /**
+     * Find data
+     * 
+     * @return Response
+     */
+    @GET
+    @Produces("application/xml")
+    @Path("data/{flowRef}/{key}/{providerRef}")
+    Response findData(@PathParam("flowRef") String flowRef, @PathParam("key") String key, @PathParam("providerRef") String providerRef, @DefaultValue("full") @QueryParam("detail") String detail);
 }
