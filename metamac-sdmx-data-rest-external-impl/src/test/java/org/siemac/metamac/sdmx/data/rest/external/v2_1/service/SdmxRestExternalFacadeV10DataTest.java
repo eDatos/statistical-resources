f3c03fe76eed53147f6d7cf2dd6df12c9cb290a1
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-08-19 13:31:34 +0000

    LLCC-114: Crear mapper, servicios y callbacks para exportar mensajes SDMX de datos http://jira.arte-consultores.com/browse/LLCC-114
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@48751 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-sdmx-data-rest-external-impl/src/test/java/org/siemac/metamac/sdmx/data/rest/external/v2_1/service/SdmxRestExternalFacadeV10DataTest.java
+++ metamac-sdmx-data-rest-external-impl/src/test/java/org/siemac/metamac/sdmx/data/rest/external/v2_1/service/SdmxRestExternalFacadeV10DataTest.java
@@ -5,14 +5,12 @@ import java.io.InputStream;
 import javax.ws.rs.core.Response;
 
 import org.apache.commons.io.IOUtils;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.siemac.metamac.core.common.exception.MetamacException;
 
 public class SdmxRestExternalFacadeV10DataTest extends SdmxRestExternalFacadeV21BaseTest {
 
     @Test
-    @Ignore
     public void testData() throws Exception {
 
         Response findData = getSdmxDataRestExternalFacadeClientXml().findData("TEST_DATA_STR_ECB_EXR_RG", null);
