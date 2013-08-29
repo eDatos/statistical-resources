3496797688c0989700a00abe89db579aee2394ed
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-08-29 10:33:09 +0000

    METAMAC-1771 Mock de DsdAttributeInstanceDto
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@48994 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-core/src/test/java/org/siemac/metamac/statistical/resources/core/utils/mocks/templates/StatisticalResourcesDtoMocks.java
+++ metamac-statistical-resources-core/src/test/java/org/siemac/metamac/statistical/resources/core/utils/mocks/templates/StatisticalResourcesDtoMocks.java
@@ -9,7 +9,9 @@ import java.util.Map;
 
 import org.apache.commons.lang.math.RandomUtils;
 import org.joda.time.DateTime;
+import org.siemac.metamac.common.test.constants.ConfigurationMockConstants;
 import org.siemac.metamac.common.test.utils.MetamacMocks;
+import org.siemac.metamac.core.common.constants.CoreCommonConstants;
 import org.siemac.metamac.core.common.dto.ExternalItemDto;
 import org.siemac.metamac.core.common.dto.InternationalStringDto;
 import org.siemac.metamac.core.common.dto.LocalisedStringDto;
@@ -28,8 +30,10 @@ import org.siemac.metamac.statistical.resources.core.dto.SiemacMetadataStatistic
 import org.siemac.metamac.statistical.resources.core.dto.StatisticalResourceDto;
 import org.siemac.metamac.statistical.resources.core.dto.VersionRationaleTypeDto;
 import org.siemac.metamac.statistical.resources.core.dto.VersionableStatisticalResourceDto;
+import org.siemac.metamac.statistical.resources.core.dto.datasets.AttributeValueDto;
 import org.siemac.metamac.statistical.resources.core.dto.datasets.DatasetVersionDto;
 import org.siemac.metamac.statistical.resources.core.dto.datasets.DatasourceDto;
+import org.siemac.metamac.statistical.resources.core.dto.datasets.DsdAttributeInstanceDto;
 import org.siemac.metamac.statistical.resources.core.dto.datasets.StatisticOfficialityDto;
 import org.siemac.metamac.statistical.resources.core.dto.publication.ChapterDto;
 import org.siemac.metamac.statistical.resources.core.dto.publication.CubeDto;
@@ -42,8 +46,8 @@ import org.siemac.metamac.statistical.resources.core.enume.domain.TypeRelatedRes
 import org.siemac.metamac.statistical.resources.core.enume.domain.VersionRationaleTypeEnum;
 import org.siemac.metamac.statistical.resources.core.enume.query.domain.QueryTypeEnum;
 
-
 public class StatisticalResourcesDtoMocks extends MetamacMocks {
+
     private static final String URN_RELATED_RESOURCE_MOCK = "urn:lorem.ipsum.dolor.infomodel.package.Resource=" + mockString(10);
 
     // -----------------------------------------------------------------
@@ -68,14 +72,38 @@ public class StatisticalResourcesDtoMocks extends MetamacMocks {
 
         return queryVersionDto;
     }
-    
+
+    // -----------------------------------------------------------------
+    // CODE ITEMS
+    // -----------------------------------------------------------------
+
+    public static CodeItemDto mockCodeItemDto() {
+        return mockCodeItemDto("code-" + mockString(5), mockString(10));
+    }
+
+    public static List<CodeItemDto> mockCodeItemDtos() {
+        List<CodeItemDto> codeItemDtos = new ArrayList<CodeItemDto>();
+        codeItemDtos.add(mockCodeItemDto());
+        codeItemDtos.add(mockCodeItemDto());
+        codeItemDtos.add(mockCodeItemDto());
+        return codeItemDtos;
+    }
+
+    private static Map<String, List<CodeItemDto>> mockCodeIdemMap() {
+        Map<String, List<CodeItemDto>> map = new HashMap<String, List<CodeItemDto>>();
+        map.put(mockString(8), mockCodeItemDtos());
+        map.put(mockString(8), mockCodeItemDtos());
+        map.put(mockString(8), mockCodeItemDtos());
+        return map;
+    }
+
     public static CodeItemDto mockCodeItemDto(String code, String title) {
         CodeItemDto codeItem = new CodeItemDto();
         codeItem.setCode(code);
         codeItem.setTitle(title);
         return codeItem;
     }
-    
+
     public static List<CodeItemDto> mockCodeItemDtosWithIdentifiers(String... identifiers) {
         List<CodeItemDto> codes = new ArrayList<CodeItemDto>();
         for (String identifier : identifiers) {
@@ -94,11 +122,48 @@ public class StatisticalResourcesDtoMocks extends MetamacMocks {
         mockIdentifiableStatisticalResourceDto(datasourceDto);
 
         // TODO: Esto debe eliminarse cuando el código pase a ser generado automáticamente
-        // code is not setting in nameable becasuse some resources have generated code
+        // code is not setting in nameable because some resources have generated code
         datasourceDto.setCode(mockString(8));
         datasourceDto.setDatasetVersionUrn(URN_RELATED_RESOURCE_MOCK);
 
         return datasourceDto;
+    }
+
+    // -----------------------------------------------------------------
+    // ATTRIBUTES
+    // -----------------------------------------------------------------
+
+    public static DsdAttributeInstanceDto mockDsdAttributeInstanceDtoWithNonNumeratedValue() {
+        DsdAttributeInstanceDto dsdAttributeInstanceDto = mockDsdAttributeInstanceDtoWithoutValue();
+        dsdAttributeInstanceDto.setValue(mockNonNumeratedAttributeValue());
+        return dsdAttributeInstanceDto;
+    }
+
+    public static DsdAttributeInstanceDto mockDsdAttributeInstanceDtoWithEnumeratedValue() {
+        DsdAttributeInstanceDto dsdAttributeInstanceDto = mockDsdAttributeInstanceDtoWithoutValue();
+        dsdAttributeInstanceDto.setValue(mockEnumeratedAttributeValue());
+        return dsdAttributeInstanceDto;
+    }
+
+    private static DsdAttributeInstanceDto mockDsdAttributeInstanceDtoWithoutValue() {
+        DsdAttributeInstanceDto dsdAttributeInstanceDto = new DsdAttributeInstanceDto();
+        dsdAttributeInstanceDto.setAttributeId("attribute_" + mockString(8));
+        dsdAttributeInstanceDto.setUuid(mockString(10));
+        dsdAttributeInstanceDto.setCodeDimensions(mockCodeIdemMap());
+        return dsdAttributeInstanceDto;
+    }
+
+    private static AttributeValueDto mockNonNumeratedAttributeValue() {
+        AttributeValueDto attributeValueDto = new AttributeValueDto();
+        attributeValueDto.setStringValue(mockString(5));
+        return attributeValueDto;
+    }
+
+    private static AttributeValueDto mockEnumeratedAttributeValue() {
+        AttributeValueDto attributeValueDto = new AttributeValueDto();
+        attributeValueDto.setExternalItemValue(mockExternalItemDto("CODE_01", null, ConfigurationMockConstants.SRM_INTERNAL_API_URL_BASE + CoreCommonConstants.URL_SEPARATOR + "URI_01",
+                "URN_01_provider", "URN_01", TypeExternalArtefactsEnum.CODE));
+        return attributeValueDto;
     }
 
     // -----------------------------------------------------------------
@@ -262,7 +327,7 @@ public class StatisticalResourcesDtoMocks extends MetamacMocks {
         lifeCycleStatisticalResourceDto.setPublicationUser(mockString(10));
 
         lifeCycleStatisticalResourceDto.setMaintainer(mockAgencyExternalItemDto());
-        
+
         // Can not mock replacedByVersion and replacesVersion, we don't know the type of resource
 
         mockVersionableStatisticalResourceDto(lifeCycleStatisticalResourceDto);
