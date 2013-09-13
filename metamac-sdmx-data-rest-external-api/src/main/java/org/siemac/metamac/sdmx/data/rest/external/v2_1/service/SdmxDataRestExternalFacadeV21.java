81b137cee85f95cf7899f780d0cc1c0f149334c9
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-09-13 11:31:13 +0000

    METAMAC-1877: API: SDMX Data - Crear servicio para buscar dataflow
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@49365 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-sdmx-data-rest-external-api/src/main/java/org/siemac/metamac/sdmx/data/rest/external/v2_1/service/SdmxDataRestExternalFacadeV21.java
+++ metamac-sdmx-data-rest-external-api/src/main/java/org/siemac/metamac/sdmx/data/rest/external/v2_1/service/SdmxDataRestExternalFacadeV21.java
@@ -10,6 +10,8 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 
+import org.sdmx.resources.sdmxml.schemas.v2_1.message.Structure;
+
 @Path("v2.1")
 public interface SdmxDataRestExternalFacadeV21 {
 
@@ -49,4 +51,30 @@ public interface SdmxDataRestExternalFacadeV21 {
     Response findData(@Context HttpHeaders headers, @PathParam("flowRef") String flowRef, @PathParam("key") String key, @PathParam("providerRef") String providerRef,
             @DefaultValue("full") @QueryParam("detail") String detail, @QueryParam("dimensionAtObservation") String dimensionAtObservation, @QueryParam("startPeriod") String startPeriod,
             @QueryParam("endPeriod") String endPeriod);
+
+    /**
+     * Find data flows, retrieves the latest version of all dataflow for all agencies.
+     * Note: this is a SDMX structure service but for this implementation this service reside here
+     * 
+     * @return Structure
+     */
+    @GET
+    @Produces("application/xml")
+    @Path("dataflow")
+    Structure findDataFlows(@DefaultValue("full") @QueryParam("detail") String detail, @DefaultValue("none") @QueryParam("references") String references);
+
+    // /**
+    // * Find data flows, retrieves the specific version of specific data consumer schemes and of specific agency.
+    // *
+    // * @param agencyID Code of agency. Keyword ALL can be used.
+    // * @param resourceID Code of data consumer scheme. Keyword ALL can be used.
+    // * @param version Version of data consumer scheme. Keyword ALL or LATEST can be used.
+    // * @return Structure
+    // */
+    // @GET
+    // @Produces("application/xml")
+    // @Path("dataflow/{agencyID}/{resourceID}/{version}")
+    // Structure findDataFlows(@PathParam("agencyID") String agencyID, @PathParam("resourceID") String resourceID, @PathParam("version") String version,
+    // @DefaultValue("full") @QueryParam("detail") String detail, @DefaultValue("none") @QueryParam("references") String references);
+
 }
