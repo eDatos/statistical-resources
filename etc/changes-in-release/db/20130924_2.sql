1eb5946751d35b61c96b18a302856317dff675af
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-09-24 10:57:33 +0000

    Añadido peq script de migracion
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@49548 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/changes-in-release/db/20130924_2.sql
+++ etc/changes-in-release/db/20130924_2.sql
@@ -5,9 +5,9 @@ alter table	TB_QUERIES_VERSIONS add  DATASET_FK NUMBER(19);
 
 ALTER TABLE TB_QUERIES_VERSIONS ADD CONSTRAINT QUERY_DATASET CHECK ( NOT ( DATASET_FK IS NULL AND DATASET_VERSION_FK IS NULL ) );
 
--- RUN THIS QUERY YOU'LL GET A LIST OF UPDATES, THEN RUNN THEM
+-- RUN THIS QUERY YOU'LL GET A LIST OF UPDATES, THEN RUN THEM
 
-SELECT 'UPDATE TB_QUERIES_VERSIONS SET DATASET_FK = '||datasetVersion.dataset_fk||', DATASET_VERSION_FK = null, STATUS = ''ACTIVE'' where id = '||queryVersion.id||';' 
+SELECT 'UPDATE TB_QUERIES_VERSIONS SET DATASET_FK = '||datasetVersion.dataset_fk||', DATASET_VERSION_FK = null, STATUS = ''ACTIVE'' where id = '||queryVersion.id||';' as EXECUTE_THIS_SENTENCE 
 FROM  tb_queries_versions queryVersion, 
       tb_datasets_versions datasetVersion,
       tb_stat_resources stat
@@ -16,7 +16,7 @@ AND datasetversion.siemac_resource_fk =  stat.ID
 and stat.last_version = 1;
 
 
-SELECT 'UPDATE TB_QUERIES_VERSIONS SET STATUS = ''DISCONTINUED'' where id = '||queryVersion.id||';' 
+SELECT 'UPDATE TB_QUERIES_VERSIONS SET STATUS = ''DISCONTINUED'' where id = '||queryVersion.id||';' as EXECUTE_THIS_SENTENCE
 FROM  tb_queries_versions queryVersion, 
       tb_datasets_versions datasetVersion,
       tb_stat_resources stat
