39ade7b9fe24fead0ac5f995a838a6f678deaa82
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-09-24 09:37:38 +0000

    Añadido peq scirpt d emigracion
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@49545 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/changes-in-release/db/20130924_2.sql
+++ etc/changes-in-release/db/20130924_2.sql
@@ -4,3 +4,22 @@ ALTER TABLE	TB_QUERIES_VERSIONS modify DATASET_VERSION_FK NUMBER(19) NULL;
 alter table	TB_QUERIES_VERSIONS add  DATASET_FK NUMBER(19);
 
 ALTER TABLE TB_QUERIES_VERSIONS ADD CONSTRAINT QUERY_DATASET CHECK ( NOT ( DATASET_FK IS NULL AND DATASET_VERSION_FK IS NULL ) );
+
+-- RUN THIS QUERY YOU'LL GET A LIST OF UPDATES, THEN RUNN THEM
+
+SELECT 'UPDATE TB_QUERIES_VERSIONS SET DATASET_FK = '||datasetVersion.dataset_fk||', DATASET_VERSION_FK = null, STATUS = ''ACTIVE'' where id = '||queryVersion.id||';' 
+FROM  tb_queries_versions queryVersion, 
+      tb_datasets_versions datasetVersion,
+      tb_stat_resources stat
+WHERE queryversion.dataset_version_fk = datasetVersion.ID
+AND datasetversion.siemac_resource_fk =  stat.ID
+and stat.last_version = 1;
+
+
+SELECT 'UPDATE TB_QUERIES_VERSIONS SET STATUS = ''DISCONTINUED'' where id = '||queryVersion.id||';' 
+FROM  tb_queries_versions queryVersion, 
+      tb_datasets_versions datasetVersion,
+      tb_stat_resources stat
+WHERE queryversion.dataset_version_fk = datasetVersion.ID
+AND datasetversion.siemac_resource_fk =  stat.ID
+and stat.last_version = 0;
