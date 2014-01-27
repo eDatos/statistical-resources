5a5eb75a05bf2956a1720ec1bf7a7822caa3e7df
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-01-27 13:33:25 +0000

    METAMAC-2093 - Creación de un componente comun de requestUtils para calculo de urls
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@50876 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/utils/PlaceRequestUtils.java
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/utils/PlaceRequestUtils.java
@@ -11,11 +11,12 @@ import org.siemac.metamac.statistical.resources.core.dto.RelatedResourceDto;
 import org.siemac.metamac.statistical.resources.core.utils.shared.StatisticalResourcesUrnParserUtils;
 import org.siemac.metamac.statistical.resources.web.client.NameTokens;
 import org.siemac.metamac.statistical.resources.web.client.PlaceRequestParams;
+import org.siemac.metamac.web.common.client.utils.CommonPlaceRequestUtils;
 
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
 import com.gwtplatform.mvp.client.proxy.PlaceRequest;
 
-public class PlaceRequestUtils {
+public class PlaceRequestUtils extends CommonPlaceRequestUtils {
 
     // ---------------------------------------------------------------------------
     // OPERATIONS
@@ -189,30 +190,6 @@ public class PlaceRequestUtils {
     // ---------------------------------------------------------------------------
     // GENERIC METHODS
     // ---------------------------------------------------------------------------
-
-    public static boolean isNameTokenInPlaceHierarchy(PlaceManager placeManager, String nameToken) {
-        for (PlaceRequest placeReq : placeManager.getCurrentPlaceHierarchy()) {
-            if (nameToken.equals(placeReq.getNameToken())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static List<PlaceRequest> getHierarchyUntilNameToken(PlaceManager placeManager, String nameToken) {
-        List<PlaceRequest> filteredHierarchy = new ArrayList<PlaceRequest>();
-        List<PlaceRequest> hierarchy = placeManager.getCurrentPlaceHierarchy();
-        boolean found = false;
-        for (int i = 0; i < hierarchy.size() && !found; i++) {
-            PlaceRequest placeReq = hierarchy.get(i);
-            if (placeReq.matchesNameToken(nameToken)) {
-                found = true;
-            }
-            filteredHierarchy.add(placeReq);
-        }
-
-        return filteredHierarchy;
-    }
 
     public static List<PlaceRequest> buildAbsoluteResourcePlaceRequest(RelatedResourceDto relatedResourceDto) {
         if (relatedResourceDto != null) {
