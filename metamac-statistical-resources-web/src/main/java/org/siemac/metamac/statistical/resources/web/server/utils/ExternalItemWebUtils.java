ddd4677e6ea6c84472bcde44626e68b352afc51d
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-02-13 10:50:43 +0000

    METAMAC-1788 - Añadir el nestedId a ExternalItem, recursos de la API...
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@51019 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/server/utils/ExternalItemWebUtils.java
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/server/utils/ExternalItemWebUtils.java
@@ -8,6 +8,8 @@ import org.siemac.metamac.rest.common.v1_0.domain.ListBase;
 import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.Agency;
 import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.Category;
 import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.Code;
+import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.Item;
+import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.Organisation;
 import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.ResourceInternal;
 import org.siemac.metamac.web.common.server.utils.DtoUtils;
 import org.siemac.metamac.web.common.shared.domain.ExternalItemsResult;
@@ -37,42 +39,32 @@ public class ExternalItemWebUtils extends org.siemac.metamac.web.common.client.u
         return externalItemDto;
     }
 
-    public static ExternalItemDto buildExternalItemDtoFromAgency(Agency agency) {
+    private static ExternalItemDto buildExternalItemDtoFromItem(Item item, TypeExternalArtefactsEnum type) {
         ExternalItemDto externalItemDto = new ExternalItemDto();
-        externalItemDto.setCode(agency.getId());
-        externalItemDto.setCodeNested(agency.getNestedId());
-        externalItemDto.setUri(agency.getSelfLink().getHref());
-        externalItemDto.setUrn(agency.getUrn());
-        externalItemDto.setUrnProvider(agency.getUrnProvider());
-        externalItemDto.setType(TypeExternalArtefactsEnum.AGENCY);
-        externalItemDto.setTitle(DtoUtils.getInternationalStringDtoFromInternationalString(agency.getName()));
-        externalItemDto.setManagementAppUrl(agency.getManagementAppLink());
+        externalItemDto.setCode(item.getId());
+        externalItemDto.setCodeNested(item.getNestedId());
+        externalItemDto.setUri(item.getSelfLink().getHref());
+        externalItemDto.setUrn(item.getUrn());
+        externalItemDto.setUrnProvider(item.getUrnProvider());
+        externalItemDto.setType(type);
+        externalItemDto.setTitle(DtoUtils.getInternationalStringDtoFromInternationalString(item.getName()));
+        externalItemDto.setManagementAppUrl(item.getManagementAppLink());
         return externalItemDto;
+    }
+
+    private static ExternalItemDto buildExternalItemDtoFromOrganisation(Organisation organisation, TypeExternalArtefactsEnum type) {
+        return buildExternalItemDtoFromItem(organisation, type);
+    }
+
+    public static ExternalItemDto buildExternalItemDtoFromAgency(Agency agency) {
+        return buildExternalItemDtoFromOrganisation(agency, TypeExternalArtefactsEnum.AGENCY);
     }
 
     public static ExternalItemDto buildExternalItemDtoFromCode(Code code) {
-        ExternalItemDto externalItemDto = new ExternalItemDto();
-        externalItemDto.setCode(code.getId());
-        externalItemDto.setCodeNested(code.getNestedId());
-        externalItemDto.setUri(code.getSelfLink().getHref());
-        externalItemDto.setUrn(code.getUrn());
-        externalItemDto.setUrnProvider(code.getUrnProvider());
-        externalItemDto.setType(TypeExternalArtefactsEnum.CODE);
-        externalItemDto.setTitle(DtoUtils.getInternationalStringDtoFromInternationalString(code.getName()));
-        externalItemDto.setManagementAppUrl(code.getManagementAppLink());
-        return externalItemDto;
+        return buildExternalItemDtoFromItem(code, TypeExternalArtefactsEnum.CODE);
     }
 
     public static ExternalItemDto buildExternalItemDtoFromCategory(Category category) {
-        ExternalItemDto externalItemDto = new ExternalItemDto();
-        externalItemDto.setCode(category.getId());
-        externalItemDto.setCodeNested(category.getNestedId());
-        externalItemDto.setUri(category.getSelfLink().getHref());
-        externalItemDto.setUrn(category.getUrn());
-        externalItemDto.setUrnProvider(category.getUrnProvider());
-        externalItemDto.setType(TypeExternalArtefactsEnum.CATEGORY);
-        externalItemDto.setTitle(DtoUtils.getInternationalStringDtoFromInternationalString(category.getName()));
-        externalItemDto.setManagementAppUrl(category.getManagementAppLink());
-        return externalItemDto;
+        return buildExternalItemDtoFromItem(category, TypeExternalArtefactsEnum.CATEGORY);
     }
 }
