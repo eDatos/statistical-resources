f7b2894d211d8c60eccaf983d774d77c6f8ea8e2
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-03-05 08:12:34 +0000

    METAMAC-2039 - [ESTADISTICAS.ARTE] No se visualiza la página 1 del nuevo portal >> Se modifican las URL para dar soporte a que las colecciones no sean singlepage.
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@51186 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/utils/MetamacPortalWebUtils.java
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/utils/MetamacPortalWebUtils.java
@@ -8,27 +8,35 @@ import org.siemac.metamac.statistical.resources.core.dto.query.QueryVersionDto;
 
 public class MetamacPortalWebUtils {
 
-    private static final String DATASETS      = "datasets";
-    private static final String PUBLICATIONS  = "collections";
-    private static final String QUERIES       = "queries";
-    private static final String URL_SEPARATOR = "/";
+    private static final String PAGE_SINGLEPAGE_RESOURCES      = "view.html";
+    private static final String PAGE_COLLECTION_RESOURCE       = "index.html";
+    private static final String DATASETS                       = "datasets";
+    private static final String QUERIES                        = "queries";
+    private static final String URL_SEPARATOR                  = "/";
+    private static final String URL_SINGLEPAGE_SEPARATOR       = "#";
+    private static final String URL_QUERY_SEPARATOR            = "?";
+    private static final String URL_QUERY_EQUALS               = "=";
+    private static final String URL_QUERY_AND                  = "&";
+    private static final String URL_QUERY_PARAMETER_RESOURCEID = "resourceId";
+    private static final String URL_QUERY_PARAMETER_AGENCYID   = "agencyId";
 
     public static String buildDatasetVersionUrl(DatasetVersionDto datasetVersionDto) {
-        return buildLifecycleResourceUrl(DATASETS, datasetVersionDto);
-    }
-
-    public static String buildPublicationVersionUrl(PublicationVersionDto publicationVersionDto) {
-        return buildLifecycleResourceUrl(PUBLICATIONS, publicationVersionDto);
+        return buildSinglepageResourceUrl(DATASETS, datasetVersionDto);
     }
 
     public static String buildQueryVersionUrl(QueryVersionDto queryVersionDto) {
-        return buildLifecycleResourceUrlWithoutVersion(QUERIES, queryVersionDto);
+        return buildSinglepageResourceUrlWithoutVersion(QUERIES, queryVersionDto);
     }
 
-    private static String buildLifecycleResourceUrl(String urlToken, LifeCycleStatisticalResourceDto lifeCycleStatisticalResourceDto) {
+    public static String buildPublicationVersionUrl(PublicationVersionDto publicationVersionDto) {
+        String urlWithoutVersion = buildPublicationUrlWithoutVersion(publicationVersionDto);
+        return urlWithoutVersion;
+    }
+
+    private static String buildSinglepageResourceUrl(String urlToken, LifeCycleStatisticalResourceDto lifeCycleStatisticalResourceDto) {
         StringBuilder builder = new StringBuilder();
 
-        String urlWithoutVersion = buildLifecycleResourceUrlWithoutVersion(urlToken, lifeCycleStatisticalResourceDto);
+        String urlWithoutVersion = buildSinglepageResourceUrlWithoutVersion(urlToken, lifeCycleStatisticalResourceDto);
         if (!StringUtils.isEmpty(urlWithoutVersion)) {
             String version = lifeCycleStatisticalResourceDto.getVersionLogic();
             builder.append(urlWithoutVersion).append(URL_SEPARATOR).append(version);
@@ -37,18 +45,52 @@ public class MetamacPortalWebUtils {
         return builder.toString();
     }
 
-    private static String buildLifecycleResourceUrlWithoutVersion(String urlToken, LifeCycleStatisticalResourceDto lifeCycleStatisticalResourceDto) {
-        StringBuilder builder = new StringBuilder();
+    private static String buildSinglepageResourceUrlWithoutVersion(String urlToken, LifeCycleStatisticalResourceDto lifeCycleStatisticalResourceDto) {
         String urlBase = CommonUtils.getMetamacPortalBaseUrl();
+        StringBuilder builder = new StringBuilder().append(urlBase);
 
         if (!StringUtils.isBlank(urlBase) && lifeCycleStatisticalResourceDto != null) {
 
             String maintainerCode = lifeCycleStatisticalResourceDto.getMaintainer() != null ? lifeCycleStatisticalResourceDto.getMaintainer().getCode() : null;
             String Code = lifeCycleStatisticalResourceDto.getCode();
 
-            builder.append(urlBase).append(URL_SEPARATOR);
-            builder.append(urlToken).append(URL_SEPARATOR);
-            builder.append(maintainerCode).append(URL_SEPARATOR);
+            if (!urlBase.endsWith(URL_SEPARATOR)) {
+                builder.append(URL_SEPARATOR);
+            }
+
+            builder.append(PAGE_SINGLEPAGE_RESOURCES);
+            builder.append(URL_SINGLEPAGE_SEPARATOR);
+            builder.append(urlToken);
+            builder.append(URL_SEPARATOR);
+            builder.append(maintainerCode);
+            builder.append(URL_SEPARATOR);
+            builder.append(Code);
+        }
+
+        return builder.toString();
+    }
+
+    private static String buildPublicationUrlWithoutVersion(LifeCycleStatisticalResourceDto lifeCycleStatisticalResourceDto) {
+        String urlBase = CommonUtils.getMetamacPortalBaseUrl();
+        StringBuilder builder = new StringBuilder().append(urlBase);
+
+        if (!StringUtils.isBlank(urlBase) && lifeCycleStatisticalResourceDto != null) {
+
+            String maintainerCode = lifeCycleStatisticalResourceDto.getMaintainer() != null ? lifeCycleStatisticalResourceDto.getMaintainer().getCode() : null;
+            String Code = lifeCycleStatisticalResourceDto.getCode();
+
+            if (!urlBase.endsWith(URL_SEPARATOR)) {
+                builder.append(URL_SEPARATOR);
+            }
+
+            builder.append(PAGE_COLLECTION_RESOURCE);
+            builder.append(URL_QUERY_SEPARATOR);
+            builder.append(URL_QUERY_PARAMETER_AGENCYID);
+            builder.append(URL_QUERY_EQUALS);
+            builder.append(maintainerCode);
+            builder.append(URL_QUERY_AND);
+            builder.append(URL_QUERY_PARAMETER_RESOURCEID);
+            builder.append(URL_QUERY_EQUALS);
             builder.append(Code);
         }
 
