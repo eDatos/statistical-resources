3591239524ab42e547fb2e8902e2c98c3df287a2
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-03-19 14:39:35 +0000

    METAMAC-1785: API interna: Movido NameTokens de la web al core http://jira.arte-consultores.com/browse/METAMAC-1785
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@51413 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/navigation/shared/NameTokens.java
@@ -0,0 +1,24 @@
+package org.siemac.metamac.statistical.resources.navigation.shared;
+
+public class NameTokens {
+
+    public static final String mainPage                   = "main";
+    public static final String operationsListPage         = "operations";
+    public static final String operationPage              = "operation";
+    public static final String operationResourcesPage     = "operationResources";
+    public static final String datasetsListPage           = "datasets";
+    public static final String datasetPage                = "dataset";
+    public static final String datasetMetadataPage        = "datasetMetadata";
+    public static final String datasetDatasourcesPage     = "datasetDatasources";
+    public static final String datasetAttributesPage      = "datasetAttributes";
+    public static final String datasetCategorisationsPage = "datasetCategorisations";
+    public static final String publicationsListPage       = "collections";
+    public static final String publicationPage            = "collection";
+    public static final String publicationMetadataPage    = "collectionMetadata";
+    public static final String publicationStructurePage   = "collectionStructure";
+    public static final String queriesListPage            = "queries";
+    public static final String queryPage                  = "query";
+
+    public static final String errorPage                  = "error";
+    public static final String unauthorizedAccessPage     = "unauthorizedAccess";
+}
