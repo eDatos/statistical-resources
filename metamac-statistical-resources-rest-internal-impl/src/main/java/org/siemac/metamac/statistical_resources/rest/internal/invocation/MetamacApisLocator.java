b4fbd416d9f65d2c6335eb7140666944fc8ac028
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-03-27 12:56:03 +0000

    Bug fix, rest_internal_impl
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@51687 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-rest-internal-impl/src/main/java/org/siemac/metamac/statistical_resources/rest/internal/invocation/MetamacApisLocator.java
+++ metamac-statistical-resources-rest-internal-impl/src/main/java/org/siemac/metamac/statistical_resources/rest/internal/invocation/MetamacApisLocator.java
@@ -16,15 +16,15 @@ public class MetamacApisLocator {
     @Autowired
     private StatisticalResourcesConfiguration configurationService;
 
-    private SrmRestInternalFacadeV10          srmRestExternalFacadeV10            = null;
+    private SrmRestInternalFacadeV10          srmRestInternalFacadeV10            = null;
 
     private CommonMetadataV1_0                commonMetadataRestExternalFacadeV10 = null;
 
     @PostConstruct
     public void initService() throws Exception {
 
-        String srmExternalApi = configurationService.retrieveSrmExternalApiUrlBase();
-        srmRestExternalFacadeV10 = JAXRSClientFactory.create(srmExternalApi, SrmRestInternalFacadeV10.class, null, true); // true to do thread safe
+        String srmInternalApi = configurationService.retrieveSrmInternalApiUrlBase();
+        srmRestInternalFacadeV10 = JAXRSClientFactory.create(srmInternalApi, SrmRestInternalFacadeV10.class, null, true); // true to do thread safe
 
         String commonMetadataExternalApi = configurationService.retrieveCommonMetadataExternalApiUrlBase();
         commonMetadataRestExternalFacadeV10 = JAXRSClientFactory.create(commonMetadataExternalApi, CommonMetadataV1_0.class, null, true); // true to do thread safe
@@ -32,10 +32,10 @@ public class MetamacApisLocator {
 
     public SrmRestInternalFacadeV10 getSrmRestExternalFacadeV10() {
         // reset thread context
-        WebClient.client(srmRestExternalFacadeV10).reset();
-        WebClient.client(srmRestExternalFacadeV10).accept("application/xml");
+        WebClient.client(srmRestInternalFacadeV10).reset();
+        WebClient.client(srmRestInternalFacadeV10).accept("application/xml");
 
-        return srmRestExternalFacadeV10;
+        return srmRestInternalFacadeV10;
     }
 
     public CommonMetadataV1_0 getCommonMetadataRestExternalFacadeV10() {
