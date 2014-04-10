856fb3d7519c2b4016d9bb092977fd480bf984b9
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-04-10 14:29:38 +0000

    METAMAC-2135 - Todas las propiedades de tipo password no deben almacenarse en texto plano
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@51962 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-sdmx-data-rest-external-impl/src/main/java/org/siemac/metamac/sdmx/data/rest/external/conf/DataConfigurationImpl.java
+++ metamac-sdmx-data-rest-external-impl/src/main/java/org/siemac/metamac/sdmx/data/rest/external/conf/DataConfigurationImpl.java
@@ -1,13 +1,7 @@
 package org.siemac.metamac.sdmx.data.rest.external.conf;
 
-import java.util.List;
-
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
 import org.siemac.metamac.core.common.exception.MetamacException;
-import org.siemac.metamac.core.common.exception.MetamacExceptionBuilder;
 import org.siemac.metamac.statistical.resources.core.conf.StatisticalResourcesConfiguration;
-import org.siemac.metamac.statistical.resources.core.error.ServiceExceptionType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -30,7 +24,7 @@ public class DataConfigurationImpl implements DataConfiguration {
     @Override
     public String retrieveMaintainerUrnDefault() throws MetamacException {
         if (maintainerUrnDefault == null) {
-            maintainerUrnDefault = retrieveProperty(DataConfigurationConstants.METAMAC_ORGANISATION_URN, Boolean.TRUE);
+            maintainerUrnDefault = configurationService.retrieveOrganisationUrn();
         }
         return maintainerUrnDefault;
     }
@@ -38,7 +32,7 @@ public class DataConfigurationImpl implements DataConfiguration {
     @Override
     public String retrieveOrganisationIDDefault() throws MetamacException {
         if (organisationIdDefault == null) {
-            organisationIdDefault = retrieveProperty(DataConfigurationConstants.METAMAC_ORGANISATION, Boolean.TRUE);
+            organisationIdDefault = configurationService.retrieveOrganisation();
         }
         return organisationIdDefault;
     }
@@ -46,7 +40,7 @@ public class DataConfigurationImpl implements DataConfiguration {
     @Override
     public String retrieveSdmxRegistryApiUrlBase() throws MetamacException {
         if (sdmxRegistryApiUrlBase == null) {
-            sdmxRegistryApiUrlBase = retrieveProperty(DataConfigurationConstants.ENDPOINT_SDMX_REGISTRY_EXTERNAL_API, Boolean.TRUE);
+            sdmxRegistryApiUrlBase = configurationService.retrieveSdmxRegistryExternalApiUrlBase();
         }
         return sdmxRegistryApiUrlBase;
     }
@@ -54,7 +48,7 @@ public class DataConfigurationImpl implements DataConfiguration {
     @Override
     public String retrieveSdmxSrmApiUrlBase() throws MetamacException {
         if (sdmxSrmApiUrlBase == null) {
-            sdmxSrmApiUrlBase = retrieveProperty(DataConfigurationConstants.ENDPOINT_SDMX_SRM_EXTERNAL_API, Boolean.TRUE);
+            sdmxSrmApiUrlBase = configurationService.retrieveSdmxSrmExternalApiUrlBase();
         }
         return sdmxSrmApiUrlBase;
     }
@@ -62,25 +56,9 @@ public class DataConfigurationImpl implements DataConfiguration {
     @Override
     public String retrieveSdmxStatisticalResourceApiUrlBase() throws MetamacException {
         if (sdmxStatisticalResourceApiUrlBase == null) {
-            sdmxStatisticalResourceApiUrlBase = retrieveProperty(DataConfigurationConstants.ENDPOINT_SDMX_STATISTICAL_RESOURCES_EXTERNAL_API, Boolean.TRUE);
+            sdmxStatisticalResourceApiUrlBase = configurationService.retrieveSdmxStatisticalResourcesExternalApiUrlBase();
         }
         return sdmxStatisticalResourceApiUrlBase;
-    }
-
-    private String retrieveProperty(String propertyName, Boolean required) throws MetamacException {
-        String propertyValue = configurationService.getProperty(propertyName);
-        if (required && StringUtils.isEmpty(propertyValue)) {
-            throw MetamacExceptionBuilder.builder().withExceptionItems(ServiceExceptionType.CONFIGURATION_PROPERTY_NOT_FOUND).withMessageParameters(propertyName).build();
-        }
-        return propertyValue;
-    }
-
-    private List<Object> retrievePropertyList(String propertyName, Boolean required) throws MetamacException {
-        List<Object> propertyValue = configurationService.getConfig().getList(propertyName);
-        if (required && CollectionUtils.isEmpty(propertyValue)) {
-            throw MetamacExceptionBuilder.builder().withExceptionItems(ServiceExceptionType.CONFIGURATION_PROPERTY_NOT_FOUND).withMessageParameters(propertyName).build();
-        }
-        return propertyValue;
     }
 
 }
