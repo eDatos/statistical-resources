64cc4d482c30d7939d87f69f505cdde5e4e43dec
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-07-22 09:01:45 +0000

    NONE - Corregidos errores para compatiblidad con GWT.
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@52810 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/security/shared/SharedConstraintsSecurityUtils.java
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/security/shared/SharedConstraintsSecurityUtils.java
@@ -1,20 +1,19 @@
 package org.siemac.metamac.statistical.resources.core.security.shared;
 
-import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.sso.client.MetamacPrincipal;
 import org.siemac.metamac.statistical.resources.core.enume.domain.ProcStatusEnum;
 
 public class SharedConstraintsSecurityUtils extends SharedSecurityUtils {
 
-    public static boolean canFindContentConstraintsForArtefact(MetamacPrincipal metamacPrincipal) throws MetamacException {
+    public static boolean canFindContentConstraintsForArtefact(MetamacPrincipal metamacPrincipal) {
         return isAnyStatisticalResourceRole(metamacPrincipal);
     }
 
-    public static boolean canCreateContentConstraint(MetamacPrincipal metamacPrincipal, String operationCode) throws MetamacException {
+    public static boolean canCreateContentConstraint(MetamacPrincipal metamacPrincipal, String operationCode) {
         return isOperationAllowed(metamacPrincipal, operationCode, PRODUCTION_ROLES);
     }
 
-    public static boolean canRetrieveContentConstraintByUrn(MetamacPrincipal metamacPrincipal, String operationCode, ProcStatusEnum procStatus) throws MetamacException {
+    public static boolean canRetrieveContentConstraintByUrn(MetamacPrincipal metamacPrincipal, String operationCode, ProcStatusEnum procStatus) {
         return canRetrieveStatisticalResource(metamacPrincipal, operationCode, procStatus);
     }
 
