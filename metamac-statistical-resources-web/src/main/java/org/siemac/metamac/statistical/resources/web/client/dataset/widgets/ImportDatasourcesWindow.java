32f94275aadf25f22be0207213771175544b5e46
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-07-24 13:02:37 +0000

    METAMAC-2236 A la hora de subir una fuente de datos, evitar que se borre el fichero a subir cuando se añaden mapeos de dimensiones.
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@52821 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/dataset/widgets/ImportDatasourcesWindow.java
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/dataset/widgets/ImportDatasourcesWindow.java
@@ -3,6 +3,7 @@ package org.siemac.metamac.statistical.resources.web.client.dataset.widgets;
 import static org.siemac.metamac.statistical.resources.web.client.StatisticalResourcesWeb.getConstants;
 import static org.siemac.metamac.statistical.resources.web.client.StatisticalResourcesWeb.getMessages;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,19 +16,25 @@ import org.siemac.metamac.statistical.resources.web.client.constants.Statistical
 import org.siemac.metamac.statistical.resources.web.client.dataset.view.handlers.DatasetDatasourcesTabUiHandlers;
 import org.siemac.metamac.statistical.resources.web.client.widgets.windows.search.SearchSingleSrmItemSchemeWindow;
 import org.siemac.metamac.statistical.resources.web.shared.utils.StatisticalResourcesSharedTokens;
+import org.siemac.metamac.web.common.client.MetamacWebCommon;
 import org.siemac.metamac.web.common.client.utils.RecordUtils;
 import org.siemac.metamac.web.common.client.widgets.ImportResourceWindow;
 import org.siemac.metamac.web.common.client.widgets.InformationLabel;
 import org.siemac.metamac.web.common.client.widgets.actions.search.SearchPaginatedAction;
+import org.siemac.metamac.web.common.client.widgets.form.CustomDynamicForm;
+import org.siemac.metamac.web.common.client.widgets.form.fields.CustomButtonItem;
 import org.siemac.metamac.web.common.client.widgets.form.fields.external.SearchExternalItemLinkItem;
 import org.siemac.metamac.web.common.shared.criteria.SrmExternalResourceRestCriteria;
 
+import com.smartgwt.client.widgets.form.fields.FormItem;
 import com.smartgwt.client.widgets.form.fields.HiddenItem;
 import com.smartgwt.client.widgets.form.fields.StaticTextItem;
 import com.smartgwt.client.widgets.form.fields.events.ClickEvent;
 import com.smartgwt.client.widgets.form.fields.events.ClickHandler;
 
 public class ImportDatasourcesWindow extends ImportResourceWindow {
+
+    private UploadDatasourceWithDimensionsForm           uploadDatasourceWithDimensionsForm;
 
     private Map<String, SearchSingleSrmItemSchemeWindow> dimensionWindowsMap             = new HashMap<String, SearchSingleSrmItemSchemeWindow>();
     private DatasetDatasourcesTabUiHandlers              uiHandlers;
@@ -42,8 +49,11 @@ public class ImportDatasourcesWindow extends ImportResourceWindow {
         informationLabel.setMargin(10);
         body.addMember(informationLabel);
 
-        UploadDatasourceForm form = new UploadDatasourceForm(dimensionsMapping);
-        setForm(form);
+        UploadDatasourceForm uploadDatasourceForm = new UploadDatasourceForm(dimensionsMapping);
+        setForm(uploadDatasourceForm);
+
+        uploadDatasourceWithDimensionsForm = new UploadDatasourceWithDimensionsForm(dimensionsMapping);
+        body.addMember(uploadDatasourceWithDimensionsForm);
     }
 
     public ImportDatasourcesWindow() {
@@ -54,36 +64,8 @@ public class ImportDatasourcesWindow extends ImportResourceWindow {
         informationLabel.setMargin(10);
         body.addMember(informationLabel);
 
-        UploadDatasourceForm form = new UploadDatasourceForm();
+        UploadDatasourceForm form = new UploadDatasourceForm(null);
         setForm(form);
-    }
-
-    @Override
-    public void setForm(UploadForm form) {
-        this.form = form;
-        form.setTarget(TARGET);
-        form.getUploadButton().addClickHandler(new com.smartgwt.client.widgets.form.fields.events.ClickHandler() {
-
-            @Override
-            public void onClick(com.smartgwt.client.widgets.form.fields.events.ClickEvent event) {
-                String displayValue = ImportDatasourcesWindow.this.form.getUploadItem().getDisplayValue();
-                if (ImportDatasourcesWindow.this.form.validate() && !StringUtils.isBlank(displayValue)) {
-                    for (String dimensionId : dimensionWindowsMap.keySet()) {
-                        SearchExternalItemLinkItem item = (SearchExternalItemLinkItem) ImportDatasourcesWindow.this.form.getItem(DIMENSION_SELECTION_NAME_PREFIX + dimensionId);
-                        if (item.getExternalItemDto() != null) {
-                            ImportDatasourcesWindow.this.form.setValue(StatisticalResourcesSharedTokens.UPLOAD_PARAM_DIM_PREFIX + dimensionId, item.getExternalItemDto().getUrn());
-                        } else {
-                            ImportDatasourcesWindow.this.form.setValue(StatisticalResourcesSharedTokens.UPLOAD_PARAM_DIM_PREFIX + dimensionId, "");
-                        }
-                    }
-
-                    ImportDatasourcesWindow.this.form.submitForm();
-                    hide();
-                    getUiHandlers().showWaitPopup();
-                }
-            }
-        });
-        body.addMember(form);
     }
 
     public void setDatasetVersion(String datasetVersionUrn) {
@@ -105,17 +87,38 @@ public class ImportDatasourcesWindow extends ImportResourceWindow {
 
     private class UploadDatasourceForm extends UploadForm {
 
-        public UploadDatasourceForm() {
+        public UploadDatasourceForm(Map<String, String> dimensionsMapping) {
             super(getConstants().datasources());
 
             HiddenItem datasetVersionUrnItem = new HiddenItem(StatisticalResourcesSharedTokens.UPLOAD_PARAM_DATASET_VERSION_URN);
             HiddenItem operationUrnItem = new HiddenItem(StatisticalResourcesSharedTokens.UPLOAD_PARAM_OPERATION_CODE);
 
-            addFieldsInThePenultimePosition(datasetVersionUrnItem, operationUrnItem);
-        }
+            List<FormItem> itemsToAdd = new ArrayList<FormItem>();
+            itemsToAdd.add(datasetVersionUrnItem);
+            itemsToAdd.add(operationUrnItem);
 
-        public UploadDatasourceForm(Map<String, String> dimensionsMapping) {
-            this();
+            if (dimensionsMapping != null && !dimensionsMapping.isEmpty()) {
+                for (String dimensionId : dimensionsMapping.keySet()) {
+                    itemsToAdd.add(new HiddenItem(getDimensionHiddenFieldName(dimensionId)));
+                }
+                setFields(uploadItem);
+                addFields(itemsToAdd.toArray(new FormItem[itemsToAdd.size()]));
+            } else {
+                addFieldsInThePenultimePosition(itemsToAdd.toArray(new FormItem[itemsToAdd.size()]));
+            }
+        }
+    }
+
+    private class UploadDatasourceWithDimensionsForm extends CustomDynamicForm {
+
+        public UploadDatasourceWithDimensionsForm(Map<String, String> dimensionsMapping) {
+            setAutoHeight();
+            setWidth100();
+            setWidth(ImportDatasourcesWindow.this.getWidth());
+            setMargin(8);
+            setNumCols(2);
+            setCellPadding(4);
+            setWrapItemTitles(false);
 
             if (!dimensionsMapping.isEmpty()) {
                 StaticTextItem mappingLabel = new StaticTextItem("header");
@@ -123,23 +126,46 @@ public class ImportDatasourcesWindow extends ImportResourceWindow {
                 mappingLabel.setValue(getMessages().datasourceImportationDimensionMapping());
                 mappingLabel.setColSpan(2);
 
-                addFieldsInThePenultimePosition(mappingLabel);
+                addFields(mappingLabel);
 
                 for (String dimensionId : dimensionsMapping.keySet()) {
                     addItemsForDimension(dimensionId, dimensionsMapping.get(dimensionId));
                 }
             }
+
+            CustomButtonItem uploadButton = new CustomButtonItem("button-import", MetamacWebCommon.getConstants().accept());
+            uploadButton.addClickHandler(new com.smartgwt.client.widgets.form.fields.events.ClickHandler() {
+
+                @Override
+                public void onClick(com.smartgwt.client.widgets.form.fields.events.ClickEvent event) {
+                    String displayValue = ImportDatasourcesWindow.this.form.getUploadItem().getDisplayValue();
+                    if (ImportDatasourcesWindow.this.form.validate() && !StringUtils.isBlank(displayValue)) {
+                        for (String dimensionId : dimensionWindowsMap.keySet()) {
+                            SearchExternalItemLinkItem item = (SearchExternalItemLinkItem) UploadDatasourceWithDimensionsForm.this.getItem(getDimensionSelectionFieldName(dimensionId));
+                            if (item.getExternalItemDto() != null) {
+                                ImportDatasourcesWindow.this.form.setValue(getDimensionHiddenFieldName(dimensionId), item.getExternalItemDto().getUrn());
+                            } else {
+                                ImportDatasourcesWindow.this.form.setValue(getDimensionHiddenFieldName(dimensionId), StringUtils.EMPTY);
+                            }
+                        }
+
+                        ImportDatasourcesWindow.this.form.submitForm();
+                        ImportDatasourcesWindow.this.hide();
+                        getUiHandlers().showWaitPopup();
+                    }
+                }
+            });
+            addFields(uploadButton);
         }
 
         private void addItemsForDimension(String dimensionId, String variableUrn) {
             SearchExternalItemLinkItem item = createItemForDimension(dimensionId, variableUrn);
-            HiddenItem hiddenItem = new HiddenItem(StatisticalResourcesSharedTokens.UPLOAD_PARAM_DIM_PREFIX + dimensionId);
-            addFieldsInThePenultimePosition(item, hiddenItem);
+            addFields(item);
         }
 
         private SearchExternalItemLinkItem createItemForDimension(final String dimensionId, final String variableUrn) {
 
-            final String name = DIMENSION_SELECTION_NAME_PREFIX + dimensionId;
+            final String name = getDimensionSelectionFieldName(dimensionId);
             String title = dimensionId;
 
             final SearchExternalItemLinkItem item = new SearchExternalItemLinkItem(name, title) {
@@ -153,7 +179,6 @@ public class ImportDatasourcesWindow extends ImportResourceWindow {
                                 public void retrieveResultSet(int firstResult, int maxResults, SrmExternalResourceRestCriteria criteria) {
                                     getUiHandlers().retrieveAlternativeCodelistsForVariable(dimensionId, variableUrn, firstResult, maxResults, criteria);
                                 }
-
                             });
 
                     getUiHandlers().retrieveAlternativeCodelistsForVariable(dimensionId, variableUrn, 0, StatisticalResourceWebConstants.FORM_LIST_MAX_RESULTS, searchWindow.getSearchCriteria());
@@ -164,8 +189,8 @@ public class ImportDatasourcesWindow extends ImportResourceWindow {
                         public void onClick(ClickEvent event) {
                             ExternalItemDto selectedResource = searchWindow.getSelectedResource();
                             searchWindow.markForDestroy();
-                            ImportDatasourcesWindow.this.form.setValue(name, RecordUtils.getExternalItemRecord(selectedResource));
-                            ImportDatasourcesWindow.this.form.getItem(name).validate();
+                            UploadDatasourceWithDimensionsForm.this.setValue(name, RecordUtils.getExternalItemRecord(selectedResource));
+                            UploadDatasourceWithDimensionsForm.this.getItem(name).validate();
                         }
                     });
                     dimensionWindowsMap.put(dimensionId, searchWindow);
@@ -179,8 +204,28 @@ public class ImportDatasourcesWindow extends ImportResourceWindow {
         return uiHandlers;
     }
 
+    private String getDimensionHiddenFieldName(String dimensionId) {
+        return StatisticalResourcesSharedTokens.UPLOAD_PARAM_DIM_PREFIX + dimensionId;
+    }
+
+    private String getDimensionSelectionFieldName(String dimensionId) {
+        return DIMENSION_SELECTION_NAME_PREFIX + dimensionId;
+    }
+
     public void setUiHandlers(DatasetDatasourcesTabUiHandlers uiHandlers) {
         this.uiHandlers = uiHandlers;
+    }
+
+    @Override
+    public void show() {
+        if (uploadDatasourceWithDimensionsForm != null) {
+            for (FormItem formItem : uploadDatasourceWithDimensionsForm.getFields()) {
+                if (StringUtils.startsWith(formItem.getName(), DIMENSION_SELECTION_NAME_PREFIX)) {
+                    formItem.clearValue();
+                }
+            }
+        }
+        super.show();
     }
 
     @Override
