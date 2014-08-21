f0218a6b47d02fabfe4f14ccd1b94a638703f6d2
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-08-21 09:39:13 +0000

    SONAR - Evitar modificadores redundantes
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@52992 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/dataset/criteria/mapper/DatasetVersionMetamacCriteria2SculptorCriteriaMapper.java
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/dataset/criteria/mapper/DatasetVersionMetamacCriteria2SculptorCriteriaMapper.java
@@ -3,8 +3,7 @@ package org.siemac.metamac.statistical.resources.core.dataset.criteria.mapper;
 import org.siemac.metamac.core.common.criteria.mapper.MetamacCriteria2SculptorCriteria;
 import org.siemac.metamac.statistical.resources.core.dataset.domain.DatasetVersion;
 
-
 public interface DatasetVersionMetamacCriteria2SculptorCriteriaMapper {
-    
-    public MetamacCriteria2SculptorCriteria<DatasetVersion> getDatasetVersionCriteriaMapper();
+
+    MetamacCriteria2SculptorCriteria<DatasetVersion> getDatasetVersionCriteriaMapper();
 }
