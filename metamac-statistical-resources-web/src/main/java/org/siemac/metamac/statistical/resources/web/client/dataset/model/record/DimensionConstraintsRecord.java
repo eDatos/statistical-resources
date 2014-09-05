eda066e68fd5a1c0c731e1a299bc557672ee7c8c
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-09-05 10:12:41 +0000

    METAMAC-1985 Añadir constraints a los DSD para simplificar la creación y reutilización de los mismos >> Edición de KeyValues con valores no enumerados
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@53191 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/dataset/model/record/DimensionConstraintsRecord.java
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/dataset/model/record/DimensionConstraintsRecord.java
@@ -1,14 +1,19 @@
 package org.siemac.metamac.statistical.resources.web.client.dataset.model.record;
 
+import static org.siemac.metamac.statistical.resources.web.client.StatisticalResourcesWeb.getConstants;
 import static org.siemac.metamac.statistical.resources.web.client.StatisticalResourcesWeb.getCoreMessages;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.siemac.metamac.core.common.util.shared.BooleanUtils;
+import org.siemac.metamac.core.common.util.shared.StringUtils;
 import org.siemac.metamac.statistical.resources.core.dto.constraint.KeyPartDto;
 import org.siemac.metamac.statistical.resources.core.dto.datasets.DsdDimensionDto;
+import org.siemac.metamac.statistical.resources.core.enume.constraint.domain.KeyPartTypeEnum;
 import org.siemac.metamac.statistical.resources.web.client.dataset.model.ds.DimensionConstraintsDS;
+import org.siemac.metamac.statistical.resources.web.client.utils.CommonUtils;
+import org.siemac.metamac.statistical.resources.web.shared.dtos.RangeDto;
 import org.siemac.metamac.web.common.client.utils.CommonWebUtils;
 
 import com.smartgwt.client.widgets.grid.ListGridRecord;
@@ -28,10 +33,13 @@ public class DimensionConstraintsRecord extends ListGridRecord {
     }
 
     public void setValues(List<KeyPartDto> keyParts) {
-        // TODO METAMAC-1985 how to set values when the dimension associated does not have a enumerated representation?
         List<String> values = new ArrayList<String>(keyParts.size());
         for (KeyPartDto keyPart : keyParts) {
-            values.add(keyPart.getValue());
+            if (KeyPartTypeEnum.NORMAL.equals(keyPart.getType())) {
+                values.add(keyPart.getValue());
+            } else if (KeyPartTypeEnum.TIME_RANGE.equals(keyPart.getType())) {
+                values.add(getKeyPartRangeValue(keyPart));
+            }
         }
         setAttribute(DimensionConstraintsDS.VALUES, CommonWebUtils.getStringListToString(values));
     }
@@ -42,5 +50,17 @@ public class DimensionConstraintsRecord extends ListGridRecord {
 
     public DsdDimensionDto getDimensionDto() {
         return (org.siemac.metamac.statistical.resources.core.dto.datasets.DsdDimensionDto) getAttributeAsObject(DimensionConstraintsDS.DSD_DIMENSION_DTO);
+    }
+
+    private String getKeyPartRangeValue(KeyPartDto keyPartDto) {
+        RangeDto rangeDto = CommonUtils.buildRangeDto(keyPartDto);
+        StringBuilder value = new StringBuilder();
+        if (!StringUtils.isBlank(rangeDto.getFromValue())) {
+            value.append(getConstants().datasetConstraintRangeFrom()).append(" ").append(rangeDto.getFromValue()).append(" ");
+        }
+        if (!StringUtils.isBlank(rangeDto.getToValue())) {
+            value.append(getConstants().datasetConstraintRangeTo()).append(" ").append(rangeDto.getToValue()).append(" ");
+        }
+        return value.toString();
     }
 }
