f058306330ec8bc5fd789b3335dfbf105d0fd39a
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-09-15 07:47:35 +0000

    METAMAC-2352 [WEB INTERNA] Mostrar el spinner "Cargando" durante las subidas de ficheros
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@53257 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/dataset/widgets/ImportDatasourcesWindow.java
+++ metamac-statistical-resources-web/src/main/java/org/siemac/metamac/statistical/resources/web/client/dataset/widgets/ImportDatasourcesWindow.java
@@ -231,4 +231,9 @@ public class ImportDatasourcesWindow extends ImportResourceWindow {
     public String getRelativeURL(String url) {
         return StatisticalResourcesWeb.getRelativeURL(url);
     }
+    
+    @Override
+    public void showWaitPopup() {
+        // no need to show the waitPopup here
+    }
 }
