06eb358c157136808b14d6c52f3a95aee3dcc4d6
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-10-09 13:23:33 +0000

    METAMAC-2365 - Corregir errores en tests que evitan que el Jenkins pase correctamente
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-resources/trunk/metamac-statistical-resources@53446 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-resources-rest-internal-impl/src/main/java/org/siemac/metamac/statistical_resources/rest/internal/v1_0/mapper/base/CommonDo2RestMapperV10Impl.java
+++ metamac-statistical-resources-rest-internal-impl/src/main/java/org/siemac/metamac/statistical_resources/rest/internal/v1_0/mapper/base/CommonDo2RestMapperV10Impl.java
@@ -1697,6 +1697,6 @@ public class CommonDo2RestMapperV10Impl implements CommonDo2RestMapperV10 {
 
     private InternationalString getUpdatedStatisticalOperationName(String operationCode) {
         Operation operation = statisticalOperationsRestInternalFacade.retrieveOperation(operationCode);
-        return operation.getName();
+        return operation != null ? operation.getName() : null;
     }
 }
