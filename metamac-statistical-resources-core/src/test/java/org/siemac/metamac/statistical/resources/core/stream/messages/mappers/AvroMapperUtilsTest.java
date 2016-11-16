75f1b97def9d1fd96ac02b57e249791c2bd0fe56
Fernando M. Gonzalez Morales <fgonmor@arte-consultores.com>
2016-11-16 08:03:23 +0000

    METAMAC-2504
    Arreglando tests

--- metamac-statistical-resources-core/src/test/java/org/siemac/metamac/statistical/resources/core/stream/messages/mappers/AvroMapperUtilsTest.java
+++ metamac-statistical-resources-core/src/test/java/org/siemac/metamac/statistical/resources/core/stream/messages/mappers/AvroMapperUtilsTest.java
@@ -1,7 +1,5 @@
 package org.siemac.metamac.statistical.resources.core.stream.messages.mappers;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -13,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.siemac.metamac.core.common.conf.ConfigurationService;
 import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.statistical.resources.core.base.domain.IdentifiableStatisticalResource;
 import org.siemac.metamac.statistical.resources.core.dataset.domain.DatasetRepository;
@@ -23,18 +22,29 @@ import org.siemac.metamac.statistical.resources.core.stream.messages.Identifiabl
 import org.siemac.metamac.statistical.resources.core.utils.asserts.BaseAsserts;
 import org.siemac.metamac.statistical.resources.core.utils.asserts.DatasetsAsserts;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
 public class AvroMapperUtilsTest {
 
     @Mock
     DatasetRepository datasetRepository;
     @Mock
     DatasetVersionRepository datasetVersionRepository;
+    @Mock
+    ConfigurationService     configurationService;
 
     @Before
     public void setUp() throws MetamacException {
         MockitoAnnotations.initMocks(this);
         AvroMapperUtils.setDatasetRepository(datasetRepository);
         AvroMapperUtils.setDatasetVersionRepository(datasetVersionRepository);
+        AvroMapperUtils.setConfiguratinService(configurationService);
+
+        try {
+            when(configurationService.retrieveStatisticalResourcesInternalApiUrlBase()).thenReturn(MappersMockUtils.EXPECTED_API_BASE);
+        } catch (MetamacException e) {
+        }
     }
 
 
