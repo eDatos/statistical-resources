72004dae667b0cbad570435ea1d30ca38b480969
acidcar <acidcar@arte-consultores.com>
2017-06-27 14:28:24 +0100

    METAMAC-2624 - Crear tarea para despliegue de todo metamac

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -5,6 +5,12 @@ TRANSFER_PATH=$HOME_PATH/tmp
 DEPLOY_TARGET_PATH=/servers/metamac/tomcats/metamac01/webapps
 ENVIRONMENT_RELATIVE_PATH_FILE=WEB-INF/classes/metamac/environment.xml
 LOGBACK_RELATIVE_PATH_FILE=WEB-INF/classes/logback.xml
+RESTART=1
+
+if [ "$1" == "--no-restart" ]; then
+    RESTART=0
+fi
+
 
 scp -r etc/deploy deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH
 scp metamac-statistical-resources-web/target/statistical-resources-internal-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/statistical-resources-internal.war
@@ -13,42 +19,46 @@ ssh deploy@estadisticas.arte-consultores.com <<EOF
 
     chmod a+x $TRANSFER_PATH/deploy/*.sh;
     . $TRANSFER_PATH/deploy/utilities.sh
-    
-    sudo service metamac01 stop
-    checkPROC "metamac"
-    
+
+    if [ $RESTART -eq 1 ]; then
+        sudo service metamac01 stop
+        checkPROC "metamac"
+    fi
+
     ###
     # STATISTICAL-RESOURCES-INTERNAL
     ###
-    
+
     # Update Process
     sudo rm -rf $DEPLOY_TARGET_PATH/statistical-resources-internal
     sudo mv $TRANSFER_PATH/statistical-resources-internal.war $DEPLOY_TARGET_PATH/statistical-resources-internal.war
     sudo unzip $DEPLOY_TARGET_PATH/statistical-resources-internal.war -d $DEPLOY_TARGET_PATH/statistical-resources-internal
     sudo rm -rf $DEPLOY_TARGET_PATH/statistical-resources-internal.war
-    
+
     # Restore Configuration
     sudo cp $HOME_PATH/environment_internal.xml $DEPLOY_TARGET_PATH/statistical-resources-internal/$ENVIRONMENT_RELATIVE_PATH_FILE
     sudo cp $HOME_PATH/logback_internal.xml $DEPLOY_TARGET_PATH/statistical-resources-internal/$LOGBACK_RELATIVE_PATH_FILE
-    
-    
+
+
     ###
     # STATISTICAL-RESOURCES-EXTERNAL
     ###
-    
+
     # Update Process
     sudo rm -rf $DEPLOY_TARGET_PATH/statistical-resources
     sudo mv $TRANSFER_PATH/statistical-resources.war $DEPLOY_TARGET_PATH/statistical-resources.war
     sudo unzip $DEPLOY_TARGET_PATH/statistical-resources.war -d $DEPLOY_TARGET_PATH/statistical-resources
     sudo rm -rf $DEPLOY_TARGET_PATH/statistical-resources.war
-    
+
     # Restore Configuration
     sudo cp $HOME_PATH/environment.xml $DEPLOY_TARGET_PATH/statistical-resources/$ENVIRONMENT_RELATIVE_PATH_FILE
     sudo cp $HOME_PATH/logback.xml $DEPLOY_TARGET_PATH/statistical-resources/$LOGBACK_RELATIVE_PATH_FILE
-    
-    sudo chown -R metamac.metamac /servers/metamac
-    sudo service metamac01 start
-    
+
+    if [ $RESTART -eq 1 ]; then
+        sudo chown -R metamac.metamac /servers/metamac
+        sudo service metamac01 start
+    fi
+
     #checkURL "http://estadisticas.arte-consultores.com/statistical-resources-internal" "metamac01"
     #checkURL "http://estadisticas.arte-consultores.com/statistical-resources/latest" "metamac01"
     echo "Finished deploy"
