d2fe2a5760688ecc6b5f8c51659b4cba50af6f43
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2018-04-18 13:40:47 +0100

    METAMAC-2718 - Eliminar código muerto para evitar mantenerlo

--- metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/publication/utils/PublicationVersioningCopyUtils.java
+++ metamac-statistical-resources-core/src/main/java/org/siemac/metamac/statistical/resources/core/publication/utils/PublicationVersioningCopyUtils.java
@@ -47,7 +47,7 @@ public class PublicationVersioningCopyUtils {
             target.setPublicationVersionFirstLevel(publicationVersionTarget);
             publicationVersionTarget.getChildrenFirstLevel().add(target);
             publicationVersionTarget.getChildrenAllLevels().add(target);
-            targets.add(target);
+            targets.add(target); // FIXME METAMAC-2718 - Eliminar código muerto para evitar mantenerlo (revisar, esto parece innecesario)
         }
     }
 
