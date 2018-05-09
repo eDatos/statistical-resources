eb1c64f1bbdf6d3ad773df1065becd3c3c5ec4cc
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2018-05-09 08:14:38 +0100

    METAMAC-2720 - Exponer los multidatasets a través de la API > Añadida excepción

--- metamac-statistical-resources-rest-internal-impl/src/main/java/org/siemac/metamac/statistical_resources/rest/internal/exception/RestServiceExceptionType.java
+++ metamac-statistical-resources-rest-internal-impl/src/main/java/org/siemac/metamac/statistical_resources/rest/internal/exception/RestServiceExceptionType.java
@@ -7,4 +7,5 @@ public class RestServiceExceptionType extends RestCommonServiceExceptionType {
     public static final RestCommonServiceExceptionType DATASET_NOT_FOUND    = create("exception.statistical_resources.dataset.not_found");
     public static final RestCommonServiceExceptionType COLLECTION_NOT_FOUND = create("exception.statistical_resources.collection.not_found");
     public static final RestCommonServiceExceptionType QUERY_NOT_FOUND      = create("exception.statistical_resources.query.not_found");
+    public static final RestCommonServiceExceptionType MULTIDATASET_NOT_FOUND = create("exception.statistical_resources.multidataset.not_found");
 }
