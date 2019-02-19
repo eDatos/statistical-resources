a4a0df2c3a5f4200ecadeda5eab80dbfedb97dfa
Miguel Jonás Bravo García <mbragar@arte-consultores.com>
2019-02-19 13:17:25 +0000

    METAMAC-2878 Permitir indicar el nombre que se quiere que tome la dimensión de filtrado
    > Modificada la generación de mocks para tests para que tengan en cuenta el nuevo campo
    > Incluidos nuevos mensajes relacionados con el campo creado
    > Añadidos nuevos tests para validar en la fachada que el nuevo campo esté informado
    > Modificados scripts sql para eliminar el esquema de las tablas

--- etc/changes-from-release/3.1.1/db/statistical-resources/oracle/20180215-update_filtering_dimension.sql
+++ etc/changes-from-release/3.1.1/db/statistical-resources/oracle/20180215-update_filtering_dimension.sql
@@ -8,29 +8,29 @@ BEGIN
             tmv.id   AS tmv_id,
             tis.id   AS tis_id
         FROM
-            metamac_statistical_resources.tb_multidatasets_versions tmv,
-            metamac_statistical_resources.tb_stat_resources tsr,
-            metamac_statistical_resources.tb_international_strings tis
+            tb_multidatasets_versions tmv,
+            tb_stat_resources tsr,
+            tb_international_strings tis
         WHERE
             tsr.type = 'MULTIDATASET'
             AND tsr.title_fk = tis.id
             AND tmv.siemac_resource_fk = tsr.id
     ) LOOP
-        INSERT INTO metamac_statistical_resources.tb_international_strings (id, version) VALUES (seq_i18nstrs.NEXTVAL, 1);
+        INSERT INTO tb_international_strings (id, version) VALUES (seq_i18nstrs.NEXTVAL, 1);
 
         FOR strings IN (
             SELECT
                 tls.label,
                 tls.locale
             FROM
-                metamac_statistical_resources.tb_localised_strings tls
+                tb_localised_strings tls
             WHERE
                 tls.international_string_fk = tb_stat_resources_rec.tis_id
         ) LOOP
-            INSERT INTO metamac_statistical_resources.tb_localised_strings (id, label, locale, international_string_fk, version) VALUES (seq_l10nstrs.NEXTVAL, strings.label, strings.locale, seq_i18nstrs.CURRVAL, 1);
+            INSERT INTO tb_localised_strings (id, label, locale, international_string_fk, version) VALUES (seq_l10nstrs.NEXTVAL, strings.label, strings.locale, seq_i18nstrs.CURRVAL, 1);
         END LOOP;
 
-        UPDATE metamac_statistical_resources.tb_multidatasets_versions SET filtering_dimension_fk = seq_i18nstrs.CURRVAL WHERE id = tb_stat_resources_rec.tmv_id;
+        UPDATE tb_multidatasets_versions SET filtering_dimension_fk = seq_i18nstrs.CURRVAL WHERE id = tb_stat_resources_rec.tmv_id;
 
         COMMIT;
     END LOOP;
