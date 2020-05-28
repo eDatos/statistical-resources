a83e8fa312a1cc410cb111e4d70fdbf02aac8d2b
Miguel Jonás Bravo García <mbragar@arte-consultores.com>
2020-05-28 09:39:11 +0100

    EDATOS-3146 - Instalación del entorno de estadísticas en la nueva máquina OVH
    > Se sustituye en los scripts de despliegue la IP de la nueva máquina por el nombre de la misma

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -12,10 +12,10 @@ if [ "$1" == "--no-restart" ]; then
     RESTART=0
 fi
 
-scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r etc/deploy deploy@192.168.10.16:$TRANSFER_PATH
-scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" metamac-statistical-resources-web/target/statistical-resources-internal-*.war deploy@192.168.10.16:$TRANSFER_PATH/statistical-resources-internal.war
-scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" metamac-statistical-resources-external-web/target/statistical-resources-*.war deploy@192.168.10.16:$TRANSFER_PATH/statistical-resources.war
-ssh -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" deploy@192.168.10.16 <<EOF
+scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r etc/deploy deploy@estadisticas.arte.internal:$TRANSFER_PATH
+scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" metamac-statistical-resources-web/target/statistical-resources-internal-*.war deploy@estadisticas.arte.internal:$TRANSFER_PATH/statistical-resources-internal.war
+scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" metamac-statistical-resources-external-web/target/statistical-resources-*.war deploy@estadisticas.arte.internal:$TRANSFER_PATH/statistical-resources.war
+ssh -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" deploy@estadisticas.arte.internal <<EOF
 
     chmod a+x $TRANSFER_PATH/deploy/*.sh;
     . $TRANSFER_PATH/deploy/utilities.sh
