9e04773de02c37737ee86a70f0257b1e4b7f4763
Miguel Jonás Bravo García <mbragar@arte-consultores.com>
2021-09-14 10:38:50 +0100

    EDATOS-3304 - Entregas ISTAC 2021 - edatos
    > Corregida errata detectada al instalar en PRE del ISTAC

--- etc/changes-from-release/3.10.0/db/statistical-resources/postgresql/20210908_adding_indexes.sql
+++ etc/changes-from-release/3.10.0/db/statistical-resources/postgresql/20210908_adding_indexes.sql
@@ -9,7 +9,7 @@
 -- en memoria a medida que detecta la necesidad.
 -- ---------------------------------------------------------------------------------------------------
 
-CREATE INDEX tb_code_dimensions_dataset_version_fk_idx ON metamac_statistical_resources_bd.tb_code_dimensions USING btree (dataset_version_fk);
-CREATE INDEX tb_code_dimensions_dsd_component_id_idx ON metamac_statistical_resources_bd.tb_code_dimensions USING btree (dsd_component_id);
+CREATE INDEX tb_code_dimensions_dataset_version_fk_idx ON tb_code_dimensions USING btree (dataset_version_fk);
+CREATE INDEX tb_code_dimensions_dsd_component_id_idx ON tb_code_dimensions USING btree (dsd_component_id);
 
 commit;
